4bbad6b9e1b0edde5e773aa8c852f8dc1b06a7c3
coderaiser <coderaiser@cloudcmd.io>
2018-12-05 19:51:15 +0200

    feature(putout) add raw-full

--- bin/putout.js
+++ bin/putout.js
@@ -11,26 +11,30 @@ const chalk = require('chalk');
 const argv = require('yargs-parser')(process.argv.slice(2), {
     boolean: [
         'fix',
-        'vars',
+        'raw',
+        'raw-full',
     ]
 });
 
 const {
     fix,
     raw,
+    rawFull,
 } = argv;
 
 const files = argv._;
 
 const putout = require('..');
 const getVars = require('../lib/get-vars');
+const transform = require('../lib/transform');
+const getUnused = require('../lib/get-unused');
 
 files.forEach(processFiles);
 
 function processFiles(name) {
     const input = readFileSync(name, 'utf8');
     
-    if (raw)
+    if (raw || rawFull)
         return showRaw(input);
     
     const {code, unused} = putout(input);
@@ -47,9 +51,21 @@ function processFiles(name) {
 }
 
 function showRaw(input) {
-    const result = getVars(putout.parse(input), {
+    const vars = getVars(putout.parse(input), {
         returnPath: false,
     });
     
-    console.log(JSON.stringify(result, null, 4));
+    if (rawFull)
+        return logObj(vars);
+    
+    const transformed = transform(vars);
+    const unused = getUnused(transformed);
+    
+    logObj(unused);
 }
+
+function logObj(obj) {
+    const str = JSON.stringify(obj, null, 4);
+    console.log(str);
+}
+
