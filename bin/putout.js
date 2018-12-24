4c9bf3a5732e53de0057e85b323dba48e8de144c
coderaiser <coderaiser@cloudcmd.io>
2018-12-24 18:07:49 +0200

    feature(putout) add support of globs

--- bin/putout.js
+++ bin/putout.js
@@ -2,12 +2,26 @@
 
 'use strict';
 
+const {extname} = require('path');
+
 const {
     readFileSync,
     writeFileSync,
+    statSync,
 } = require('fs');
 
 const chalk = require('chalk');
+const glob = require('glob');
+const tryCatch = require('try-catch');
+
+const putout = require('..');
+const getVars = require('../lib/get-vars');
+const transform = require('../lib/transform');
+const getUnused = require('../lib/get-unused');
+const cutShebang = require('../lib/cut-shebang');
+
+const one = (f) => (a) => f(a);
+
 const argv = require('yargs-parser')(process.argv.slice(2), {
     boolean: [
         'fix',
@@ -22,15 +36,14 @@ const {
     rawFull,
 } = argv;
 
-const files = argv._;
+const [e, files] = tryCatch(getFiles, argv._);
 
-const putout = require('..');
-const getVars = require('../lib/get-vars');
-const transform = require('../lib/transform');
-const getUnused = require('../lib/get-unused');
-const cutShebang = require('../lib/cut-shebang');
+if (e) {
+    console.error(e);
+    process.exit(1);
+}
 
-files.forEach(processFiles);
+files.map(processFiles);
 
 function processFiles(name) {
     const input = readFileSync(name, 'utf8');
@@ -68,5 +81,23 @@ function showRaw(input) {
 function logObj(obj) {
     const str = JSON.stringify(obj, null, 4);
     console.log(str);
+}
+
+function addExt(a) {
+    const ext = extname(a);
+    const isDir = statSync(a).isDirectory();
+    
+    if (isDir && ext !== '.js')
+        return `${a}/**/*.js`;
+    
+    return a;
+};
+
+function getFiles(args) {
+    const files = args
+        .map(addExt)
+        .map(one(glob.sync));
+    
+    return [].concat(...files);
 }
 
