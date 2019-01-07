0804701ebc14be1baa7d57083f09e08568b1a331
coderaiser <coderaiser@cloudcmd.io>
2019-01-07 13:30:02 +0200

    feature(putout) add support of plugins load

--- packages/putout/lib/get-plugins.js
+++ packages/putout/lib/get-plugins.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const Module = require('module');
 const {join} = require('path');
 
 const deepmerge = require('deepmerge');
 const tryCatch = require('try-catch');
 
 const defaultOptions = require('../putout.json');
+
+const {cwd} = process;
 
 const addDir = (a) => join(__dirname, '..', 'lib', a);
 const isDisabled = (a) => !a && typeof a === 'boolean';
@@ -39,12 +42,24 @@ function requirePlugin(name) {
     if (localPlugin)
         return localPlugin;
     
-    const [, npmPlugin] = tryCatch(require, `@putout/plugin-${name}`);
+    const [, npmPlugin] = tryCatch(require, getModulePath(`@putout/plugin-${name}`));
     if (npmPlugin)
         return npmPlugin;
      
-    const [, userPlugin] = tryCatch(require, `putout-plugin-${name}`);
+    const [, userPlugin] = tryCatch(require, getModulePath(`putout-plugin-${name}`));
     if (userPlugin)
         return userPlugin;
 }
+
+// Module._findPath is an internal method to Node.js, then one they use to
+// lookup file paths when require() is called. So, we are hooking into the
+// exact same logic that Node.js uses.
+//
+// https://github.com/eslint/eslint/blob/v5.12.0/lib/util/module-resolver.js#L69
+const getModulePath = (name) => {
+    return Module._findPath(name, [
+        `${cwd()}/node_modules`,
+        ...module.paths
+    ]);
+};
 
