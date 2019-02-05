b37489ec78da301628f582a0628c2d859a09e8cf
coderaiser <coderaiser@cloudcmd.io>
2019-02-05 17:14:28 +0200

    chore(plugin-remove-unused-variables) madrun: test

--- packages/plugin-remove-unused-variables/.madrun.js
+++ packages/plugin-remove-unused-variables/.madrun.js
@@ -7,7 +7,7 @@ const {
 } = require('madrun');
 
 module.exports = {
-    'test': () => `tape 'test/**/*.js'`,
+    'test': () => `tape 'test/*.js'`,
     'watch:test': () => `nodemon -w lib -w test -x ${run('test')}`,
     'lint:lib': () => `eslint lib test --ignore-pattern test/fixture`,
     'lint': () => parallel(['putout', 'lint:*']),
