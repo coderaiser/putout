71971fa89ad2a31f5e6afda7e2b82bee35150672
coderaiser <coderaiser@cloudcmd.io>
2019-02-17 21:21:53 +0200

    chore(plugin-apply-destructuring) putout

--- packages/plugin-apply-destructuring/madrun.js
+++ packages/plugin-apply-destructuring/madrun.js
@@ -9,7 +9,7 @@ const {
 module.exports = {
     'test': () => `tape 'lib/**/*.spec.js' 'test/*.js'`,
     'watch:test': () => `nodemon -w lib -w test -x ${run('test')}`,
-    'lint:lib': () => `eslint lib test --ignore-pattern fixture`,
+    'lint:lib': () => `eslint lib test --ignore-pattern 'fixture'`,
     'lint': () => parallel(['putout', 'lint:*']),
     'fix:lint': () => series(['putout', 'lint:*'], '--fix'),
     'putout': () => `putout lib`,
