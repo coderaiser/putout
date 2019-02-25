6437da94e018d6f27cd02055bcab66d0921be5ec
coderaiser <coderaiser@cloudcmd.io>
2019-02-25 14:14:29 +0200

    test(remove-this) convert -> remove

--- packages/plugin-react-hooks/lib/remove-this/index.spec.js
+++ packages/plugin-react-hooks/lib/remove-this/index.spec.js
@@ -2,15 +2,15 @@
 
 /* eslint node/no-unpublished-require:0 */
 const test = require('@putout/test')(__dirname, {
-    'convert-method-assignment-to-declaration': require('.'),
+    'remove-this': require('.'),
 });
 
-test('plugin-react-hooks: convert-method-assignment-to-declaration: report', (t) => {
+test('plugin-react-hooks: remove-this: report', (t) => {
     t.report('this', `should be used "submit" instead of "this.submit"`);
     t.end();
 });
 
-test('plugin-react-hooks: convert-method-assignment-to-declaration: transform', (t) => {
+test('plugin-react-hooks: remove-this: transform', (t) => {
     t.transform('this');
     t.end();
 });
