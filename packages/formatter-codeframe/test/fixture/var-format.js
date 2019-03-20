4131a4e1727eaae29db7771ec134426d5c764e24
coderaiser <coderaiser@cloudcmd.io>
2019-03-20 12:45:44 +0200

    test(@putout/formatter-codeframe) fixture

--- packages/formatter-codeframe/test/fixture/var-format.js
+++ packages/formatter-codeframe/test/fixture/var-format.js
@@ -1,5 +1,6 @@
-[0m  [91m"t" is defined but never used[39m at var:1:6[0m
+var:1:6
 [0m[31m[1m>[22m[39m[90m 1 | [39m[36mconst[39m t [33m=[39m [32m'hello'[39m[33m;[39m[0m
+[0m [90m   | [39m     [31m[1m^[22m[39m [31m[1m"t" is defined but never used[22m[39m[0m
 [0m [90m 2 | [39m[0m
 [0m [90m 3 | [39m[0m
 
