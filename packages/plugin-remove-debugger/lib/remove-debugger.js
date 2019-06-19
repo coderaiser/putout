f0ab7114371c178553b48a38c154887abac98c5f
coderaiser <coderaiser@cloudcmd.io>
2019-06-19 16:32:54 +0300

    chore(@putout/plugin-remove-debugger) lint

--- packages/plugin-remove-debugger/lib/remove-debugger.js
+++ packages/plugin-remove-debugger/lib/remove-debugger.js
@@ -4,7 +4,7 @@ module.exports.report = () => 'Unexpected "debugger" statement';
 
 module.exports.fix = (path) => {
     path.remove();
-}
+};
 
 module.exports.traverse = ({push}) => {
     return {
