aa34418406401aa53dedec0e5d3a646bc28f83cd
coderaiser <coderaiser@cloudcmd.io>
2019-07-05 15:02:16 +0300

    test(putout) get-plugins: coverage

---
+++ packages/putout/lib/get-plugins/index.spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const test = require('supertape');
+const mockRequire = require('mock-require');
+const {reRequire, stopAll} = mockRequire;
+
+test('get-plugins', (t) => {
+    
+    mockRequire('module', {
+        _findPath: () => {},
+        plugins: {
+            'remove-unused-variables': require('@putout/plugin-remove-unused-variables'),
+        },
+    });
+    
+    const getPlugins = reRequire('.');
+    const result = getPlugins({
+        plugins: [
+            'remove-unused-variables',
+        ],
+    });
+    
+    stopAll();
+    
+    t.equal(result.length, 1);
+    t.end();
+});
+
