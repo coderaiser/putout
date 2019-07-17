6c5b6c8a3567d0a96518f1e8b5810e9fa936c4a9
coderaiser <coderaiser@cloudcmd.io>
2019-07-17 22:54:53 +0300

    chore(madrun) lint

--- packages/plugin-extract-sequence-expressions/madrun.js
+++ packages/plugin-extract-sequence-expressions/madrun.js
@@ -2,17 +2,16 @@
 
 const {
     run,
-    series,
     parallel,
 } = require('madrun');
 
 module.exports = {
     'test': () => `tape 'test/*.js'`,
     'watch:test': () => `nodemon -w lib -w test -x ${run('test')}`,
-    'lint:lib': () => `eslint lib test --ignore-pattern test/fixture`,
+    'lint:lib': () => `eslint lib test madrun.js --ignore-pattern test/fixture`,
     'lint': () => parallel(['putout', 'lint:*']),
-    'fix:lint': () => series(['putout', 'lint:*'], '--fix'),
-    'putout': () => `putout lib test`,
+    'fix:lint': () => run(['putout', 'lint:*'], '--fix'),
+    'putout': () => `putout lib test madrun.js`,
     'coverage': () => `nyc ${run('test')}`,
     'report': () => `nyc report --reporter=text-lcov | coveralls || true`,
 };
