66f5e02bcd24a896c3d74d30d57b53b5b7a0f1bb
coderaiser <coderaiser@cloudcmd.io>
2019-07-18 20:03:26 +0300

    chore(madrun) lint

--- packages/plugin-strict-mode/madrun.js
+++ packages/plugin-strict-mode/madrun.js
@@ -1,18 +1,14 @@
 'use strict';
 
-const {
-    run,
-    series,
-    parallel,
-} = require('madrun');
+const {run} = require('madrun');
 
 module.exports = {
     'test': () => `tape 'lib/**/*.spec.js' 'test/*.js'`,
     'watch:test': () => `nodemon -w lib -w test -x ${run('test')}`,
-    'lint:lib': () => `eslint lib test --ignore-pattern fixture`,
-    'lint': () => series(['putout', 'lint:*']),
-    'fix:lint': () => series(['putout', 'lint:*'], '--fix'),
-    'putout': () => `putout lib test`,
+    'lint:lib': () => `eslint lib test madrun.js --ignore-pattern fixture`,
+    'lint': () => run(['putout', 'lint:*']),
+    'fix:lint': () => run(['putout', 'lint:*'], '--fix'),
+    'putout': () => `putout lib test madrun.js`,
     'coverage': () => `nyc ${run('test')}`,
     'report': () => `nyc report --reporter=text-lcov | coveralls || true`,
 };
