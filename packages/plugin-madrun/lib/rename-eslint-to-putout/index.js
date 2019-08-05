4d0c236a3a8baf5b99e50c72a4d4832c1f80ed8f
coderaiser <coderaiser@cloudcmd.io>
2019-08-05 18:06:56 +0300

    feature(@putout/plugin-madrun) rename-eslint-to-putout: exclude calls with rulesdir

--- packages/plugin-madrun/lib/rename-eslint-to-putout/index.js
+++ packages/plugin-madrun/lib/rename-eslint-to-putout/index.js
@@ -1,39 +1,43 @@
-'use strict';
+const {
+  types: t,
+} = require('putout');
 
-module.exports.report = () => `"putout" should be used instead of "eslint"`;
+module.exports.report = (path) => `"putout" should be used instead of "eslint"`;
 
 module.exports.fix = ({eslint}) => {
     eslint.scope.rename('eslint', 'putout');
 };
 
 module.exports.traverse = ({push}) => {
-    return {
-        VariableDeclarator(path) {
-            if (!isPredefined(path.get('init')))
-                return;
-            
-            const idPath = path.get('id');
-            
-            if (!idPath.isObjectPattern())
-                return;
-            
-            const properties = idPath.get('properties');
-            const {eslint, putout} = getPredefined(properties);
-            
-            if (!eslint || putout)
-                return;
-            
-            push({
-                eslint,
-                path,
-            });
-        },
-    };
+  return {
+    VariableDeclarator(path) {
+        if (!isPredefined(path.get('init')))
+            return;
+           
+        const idPath = path.get('id');
+        if (!idPath.isObjectPattern())
+            return;
+         
+        const properties = idPath.get('properties');
+        const {eslint, putout} = getPredefined(properties);
+        
+        if (!eslint || putout)
+            return;
+        
+        if (isRulesdir(eslint.scope.bindings))
+            return;
+        
+          push({
+            eslint,
+            path,
+        })
+    }
+  };
 };
 
 function isPredefined(path) {
     return path.isIdentifier({
-        name: 'predefined',
+        name: 'predefined'
     });
 }
 
@@ -53,3 +57,27 @@ function getPredefined(properties) {
     return result;
 }
 
+function isRulesdir({eslint}) {
+    if (!eslint)
+        return false;
+    
+    const {referencePaths} = eslint;
+
+    for (const ref of referencePaths) {
+        const {parentPath} = ref;
+
+        if (!parentPath.isCallExpression())
+            continue;
+
+        const argumentsPath = parentPath.get('arguments.0')
+        if (!argumentsPath.isObjectExpression())
+            continue;
+
+        for (const {key} of argumentsPath.node.properties) {
+            if (t.isIdentifier(key, {name: 'rulesdir'}))
+                return true;
+        }
+    }
+    
+    return false;
+}
