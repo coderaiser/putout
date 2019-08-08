41381b5edd38db86e86df566636ade54356885e2
coderaiser <coderaiser@cloudcmd.io>
2019-08-08 18:14:56 +0300

    chore(webpack) exclude wrap-plugin, @babel/core from build

--- packages/putout/.webpack/webpack.config.js
+++ packages/putout/.webpack/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     plugins: [
         new webpack.IgnorePlugin({
             checkResource(context) {
-                return /fixture|jscodeshift|tape|@putout\/test/.test(context);
+                return /fixture|jscodeshift|wrap-plugin|@babel\/core|tape|@putout\/test/.test(context);
             },
         }),
     ],
