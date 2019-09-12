5526c29e9dcc4ff37638336e40f7e84d75ba8031
coderaiser <coderaiser@cloudcmd.io>
2019-09-12 16:31:03 +0300

    feature(@putout/plugin-remove-process-exit) template

--- packages/plugin-remove-process-exit/lib/remove-process-exit.js
+++ packages/plugin-remove-process-exit/lib/remove-process-exit.js
@@ -1,46 +1,13 @@
 'use strict';
 
-const {
-    isIdentifier,
-    isMemberExpression,
-    isStringLiteral,
-} = require('putout').types;
-
 module.exports.report = () => '"process.exit" should not be used';
 
 module.exports.fix = (path) => {
     path.remove();
 };
 
-module.exports.traverse = ({push}) => {
-    return {
-        CallExpression(path) {
-            const {node} = path;
-            const {callee} = node;
-            
-            if (!isMemberExpression(callee))
-                return;
-            
-            const {
-                property,
-                object,
-            } = callee;
-            
-            const name = 'process';
-            
-            if (!isIdentifier(object, {name}))
-                return;
-            
-            traverseProperty('exit', path, property, push);
-        },
-    };
-};
-
-function traverseProperty(name, path, node, fn) {
-    if (isIdentifier(node, {name}))
-        return fn(path);
-    
-    if (isStringLiteral(node, {value: name}))
-        return fn(path);
-}
+module.exports.include = () => [
+    `process.exit()`,
+    `process['exit']()`,
+];
 
