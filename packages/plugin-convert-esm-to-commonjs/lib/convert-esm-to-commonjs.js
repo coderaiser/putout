dbd3e7740de798383f0c5aaf2fb6a12b3b24b843
coderaiser <coderaiser@cloudcmd.io>
2019-09-12 17:17:47 +0300

    feature(@putout/plugin-convert-esm-to-commonjs) include

--- packages/plugin-convert-esm-to-commonjs/lib/convert-esm-to-commonjs.js
+++ packages/plugin-convert-esm-to-commonjs/lib/convert-esm-to-commonjs.js
@@ -4,17 +4,9 @@ module.exports.report = () => 'Commonjs should be used insted of ESM';
 
 module.exports.fix = require('./fix');
 
-module.exports.traverse = ({push}) => {
-    return {
-        ExportNamedDeclaration(path) {
-            push(path);
-        },
-        ExportDefaultDeclaration(path) {
-            push(path);
-        },
-        ImportDeclaration(path) {
-            push(path);
-        },
-    };
-};
+module.exports.include = () => [
+    'ExportNamedDeclaration',
+    'ExportDefaultDeclaration',
+    'ImportDeclaration',
+];
 
