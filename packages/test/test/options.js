e6f8babe1b4a2c4df4955df3913809cbc6df8c07
coderaiser <coderaiser@cloudcmd.io>
2019-09-26 23:04:15 +0300

    test(@putout/test) transformWithOptions

---
+++ packages/test/test/options.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const removeEmpty = require('@putout/plugin-remove-empty')
+
+const test = require('..')(__dirname, {
+    'remove-empty/import': removeEmpty.rules.import,
+});
+
+test('test: options', (t) => {
+    t.transformWithOptions('transform-options', {
+        ignore: [
+            "hello"
+        ]
+    });
+    t.end();
+});
+
+test('test: options: no transform', (t) => {
+    t.noTransformWithOptions('transform-options', {
+        ignore: [
+            "hello",
+            'world',
+        ]
+    });
+    
+    t.end();
+});
