98866f1ab19690bbee1663b760cc125b7fc93168
coderaiser <coderaiser@cloudcmd.io>
2019-11-01 11:28:37 +0200

    feature(@putout/plugin-remove-only) convert to replace

--- packages/plugin-remove-only/lib/remove-only.js
+++ packages/plugin-remove-only/lib/remove-only.js
@@ -2,12 +2,8 @@
 
 module.exports.report = () => '"test.only" should not be used';
 
-module.exports.fix = ({node}) => {
-    node.callee = node.callee.object;
-};
-
-module.exports.include = () => [
-    '__.only(__)',
-    '__["only"](__)',
-];
+module.exports.replace = () => ({
+    '_a.only(_b, _c)': '_a(_b, _c)',
+    '_a["only"](_b, _c)': '_a(_b, _c)',
+});
 
