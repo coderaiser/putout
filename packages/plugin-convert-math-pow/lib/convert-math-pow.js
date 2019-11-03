0801652e603e703b3d02265dff24e8370d17f7d3
coderaiser <coderaiser@cloudcmd.io>
2019-11-03 21:45:44 +0200

    feature(@putout/plugin-convert-math-pow) convert to replace

--- packages/plugin-convert-math-pow/lib/convert-math-pow.js
+++ packages/plugin-convert-math-pow/lib/convert-math-pow.js
@@ -1,23 +1,8 @@
 'use strict';
 
-const {
-    types,
-    operate,
-} = require('putout');
-
-const {replaceWith} = operate;
-const {binaryExpression} = types;
-
 module.exports.report = () => 'operator "**" should be used instead of Math.pow';
 
-module.exports.fix = (path) => {
-    const [left, right] = path.node.arguments;
-    replaceWith(path, binaryExpression('**', left, right));
-};
-
-module.exports.include = () => {
-    return [
-        'Math.pow(__)',
-    ];
-};
+module.exports.replace = () => ({
+    'Math.pow(__a, __b)': '__a ** __b',
+});
 
