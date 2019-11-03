d50c274bd94daa0ecb813cd8514b1805152f05be
coderaiser <coderaiser@cloudcmd.io>
2019-11-03 21:58:02 +0200

    feature(@putout/plugin-remove-process-exit) convert to replace

--- packages/plugin-remove-process-exit/lib/remove-process-exit.js
+++ packages/plugin-remove-process-exit/lib/remove-process-exit.js
@@ -2,12 +2,8 @@
 
 module.exports.report = () => '"process.exit" should not be used';
 
-module.exports.fix = (path) => {
-    path.remove();
-};
-
-module.exports.include = () => [
-    `process.exit()`,
-    `process['exit']()`,
-];
+module.exports.replace = () => ({
+    'process.exit()':'',
+    'process["exit"]()': '',
+});
 
