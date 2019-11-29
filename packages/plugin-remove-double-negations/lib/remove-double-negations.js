2b064e530efa048d71224cd7b0794d4ecfe68769
coderaiser <coderaiser@cloudcmd.io>
2019-11-29 17:39:59 +0200

    chore(@putout/plugin-remove-double-negations) add " "

--- packages/plugin-remove-double-negations/lib/remove-double-negations.js
+++ packages/plugin-remove-double-negations/lib/remove-double-negations.js
@@ -3,7 +3,7 @@
 module.exports.report = () => `Double negation should not be used in conditions`;
 
 module.exports.replace = () => ({
-    'if (!!__a) __b': 'if(__a) __b',
+    'if (!!__a) __b': 'if (__a) __b',
     '!!__a ? __b : __c': '__a ? __b : __c',
     '!!__a && __b' : '__a && __b',
 });
