5e7a13ac2b2b153329d3f992450918b4159b9235
coderaiser <coderaiser@cloudcmd.io>
2019-11-29 17:29:23 +0200

    fix(@putout/apply-optional-chaining) template variables

--- packages/plugin-apply-optional-chaining/lib/apply-optional-chaining.js
+++ packages/plugin-apply-optional-chaining/lib/apply-optional-chaining.js
@@ -3,7 +3,7 @@
 module.exports.report = () => 'Optional chaining should be used';
 
 module.exports.replace = () => ({
-    '__a && __a.__b && __a.__b.__c && __a.__b.__c.__d && __a.__b.__c.__d__e': '__a?.__b?.__c?.__d?.__e',
+    '__a && __a.__b && __a.__b.__c && __a.__b.__c.__d && __a.__b.__c.__d.__e': '__a?.__b?.__c?.__d?.__e',
     '__a && __a.__b && __a.__b.__c && __a.__b.__c.__d': '__a?.__b?.__c?.__d',
     '__a && __a.__b && __a.__b.__c': '__a?.__b?.__c',
     '__a && __a.__b': '__a?.__b',
