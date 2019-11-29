bd4295ccf35318d05c47da279eb0c03c1649cf41
coderaiser <coderaiser@cloudcmd.io>
2019-11-29 17:24:22 +0200

    feature(@putout/plugin-simplify-ternary) convert to replace

--- packages/plugin-simplify-ternary/lib/simplify-ternary.js
+++ packages/plugin-simplify-ternary/lib/simplify-ternary.js
@@ -1,48 +1,8 @@
 'use strict';
 
-const {
-    types,
-    operate,
-} = require('putout');
-
-const {
-    replaceWith,
-    compare,
-} = operate;
-const {LogicalExpression} = types;
-
 module.exports.report = () => 'Unnecessary use of conditional expression for default assignment';
 
-module.exports.fix = ({path, consequent, alternate}) => {
-    replaceWith(path, LogicalExpression('||', consequent, alternate));
-};
+module.exports.replace = () => ({
+    '__a ? __a : __b': '__a || __b',
+});
 
-module.exports.traverse = ({push}) => {
-    return {
-        ConditionalExpression(path) {
-            const consequentPath = path.get('consequent');
-            
-            const {
-                test,
-                consequent,
-                alternate,
-            } = path.node;
-            
-            const {name} = test;
-            
-            if (consequentPath.isIdentifier({name}))
-                return push({
-                    path,
-                    consequent,
-                    alternate,
-                });
-            
-            if (compare(test, consequent))
-                return push({
-                    path,
-                    consequent,
-                    alternate,
-                });
-        },
-    };
-};
--- packages/plugin-simplify-ternary/test/simplify-ternary.js
+++ packages/plugin-simplify-ternary/test/simplify-ternary.js
@@ -19,7 +19,11 @@ test('plugin-simplify-ternary: transform', (t) => {
     t.end();
 });
 
-test('plugin-simplify-ternary: no: transform: different test and consequent', (t) => {
-    t.noTransform('diff');
-    t.end();
-});
+test(
+    'plugin-simplify-ternary: no: transform: different test and consequent',
+    (t) => {
+        t.noTransform('diff');
+        t.end();
+    },
+);
+
