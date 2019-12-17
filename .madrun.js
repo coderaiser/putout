f9426ec46c9b61afabcdeb5fd6ef5814043482ae
coderaiser <coderaiser@cloudcmd.io>
2019-12-17 17:06:38 +0200

    feature(putout) speed up tests

---
+++ .madrun.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const {run} = require('madrun');
+
+module.exports = {
+    'test': () => `FORCE_COLOR=3 ${run('test:clean')}`,
+    'test:clean': () => 'tape \'packages/*/test/*.js\' \'packages/*/lib/**/*.spec.js\'',
+    'test:slow': () => 'FORCE_COLOR=3 lerna run test',
+    'coverage': () => `FORCE_COLOR=3 nyc ${run('test:clean')}`,
+    'coverage:slow': () => 'FORCE_COLOR=3 lerna run coverage',
+    'report': () => 'lerna run --no-bail report',
+    'lint:slow': () => 'FORCE_COLOR=3 lerna run --no-bail lint',
+    'lint:dot': () => 'putout .madrun.js',
+    'lint': () => run('lint:*'),
+    'fix:lint': () => 'lerna run --no-bail fix:lint',
+    'bootstrap': () => 'lerna bootstrap',
+};
+
