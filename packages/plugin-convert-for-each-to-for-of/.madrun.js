4ac6a38f2c53b2cdfecac5d69dc08213c4d7b65e
coderaiser <coderaiser@cloudcmd.io>
2019-12-19 17:37:38 +0200

    feature(package) madrun v5.1.0

--- packages/plugin-convert-for-each-to-for-of/.madrun.js
+++ packages/plugin-convert-for-each-to-for-of/.madrun.js
@@ -10,15 +10,16 @@ const {putout} = predefined;
 module.exports = {
     'test': () => `tape 'test/*.js'`,
     'watch:test': () => `nodemon -w lib -w test -x ${run('test')}`,
+    
     'lint': () => putout({
         names: [
             'lib',
             'test',
-            'madrun.js',
+            '.madrun.js',
         ],
     }),
+    
     'fix:lint': () => run('lint', '--fix'),
-    'putout': () => `putout lib test`,
     'coverage': () => `nyc ${run('test')}`,
     'report': () => `nyc report --reporter=text-lcov | coveralls || true`,
 };
