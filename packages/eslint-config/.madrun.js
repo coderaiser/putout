b7fe38d250b691ea7796b345363454902e9b8668
coderaiser <coderaiser@cloudcmd.io>
2020-01-14 14:44:53 +0200

    chore(@putout/eslint-config) publishOnly

---
+++ packages/eslint-config/.madrun.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {run} = require('madrun');
+
+module.exports = {
+    'lint:json': () => 'jsonlint -q .eslintrc.json',
+    'lint:js': () => 'putout .madrun.js',
+    'lint': () => run('lint:*'),
+    'fix:lint': () => run('lint', '--fix'),
+    'publishOnly': () => run('lint'),
+};
+
