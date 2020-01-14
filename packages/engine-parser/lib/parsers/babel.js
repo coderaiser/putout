88db437d29938139b38cc37e7db4751cdd105c6b
coderaiser <coderaiser@cloudcmd.io>
2020-01-14 19:01:45 +0200

    feature(@putout/engine-parser) enable BABEL_TYPES_8_BREAKING

--- packages/engine-parser/lib/parsers/babel.js
+++ packages/engine-parser/lib/parsers/babel.js
@@ -2,6 +2,13 @@
 
 const once = require('once');
 
+// stricter validation to prevent even more invalid ASTs: not only
+// from a tree shape point of view but also ensuring that nodes in
+// the correct position carry valid information. For example, starting
+// from Babel 8 t.identifier("123") will be disallowed, because 123 is
+// not a valid identifier.
+process.env.BABEL_TYPES_8_BREAKING = true;
+
 const initBabel = once(() => require('@babel/parser'));
 const clean = (a) => a.filter(Boolean);
 const getFlow = (a) => !a.indexOf('// @flow');
