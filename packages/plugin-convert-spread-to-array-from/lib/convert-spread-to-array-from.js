fd84697d7cb6241b932ff6a15396e8b404203c3e
coderaiser <coderaiser@cloudcmd.io>
2020-01-20 13:45:12 +0200

    feature(@putout/plugin-convert-spread-to-array-from) convert to replace

--- packages/plugin-convert-spread-to-array-from/lib/convert-spread-to-array-from.js
+++ packages/plugin-convert-spread-to-array-from/lib/convert-spread-to-array-from.js
@@ -1,35 +1,8 @@
 'use strict';
 
-const {operate, template} = require('putout');
-const {replaceWith} = operate;
-
-const buildArrayFrom = template(`
-  Array.from(%%argument%%);
-`);
-
 module.exports.report = () => `Array.from should be used instead of array spread`;
 
-module.exports.fix = (path) => {
-    const [element] = path.node.elements;
-    const {argument} = element;
-    
-    const ast = buildArrayFrom({
-        argument,
-    });
-    
-    replaceWith(path, ast);
-};
-
-module.exports.include = () => [
-    '[...__]',
-];
-
-module.exports.filter = (path) => {
-    const {elements} = path.node;
-    
-    if (elements.length === 1)
-        return true;
-    
-    return false;
-};
+module.exports.replace = () => ({
+    '[...__a]': 'Array.from(__a)',
+});
 
