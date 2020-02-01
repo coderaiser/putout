3936a532cd2a2fe0b7e6e09654089b738075103f
coderaiser <coderaiser@cloudcmd.io>
2020-02-01 12:47:14 +0200

    chore(madrun) get workspaces from package.json

--- .madrun.js
+++ .madrun.js
@@ -1,20 +1,28 @@
 'use strict';
 
 const {run} = require('madrun');
+const {workspaces} = require('./package');
+
+const cutStar = (a) => a.replace('/*', '');
+const dirs = getDirs(workspaces);
 
 module.exports = {
     'test': () => `FORCE_COLOR=3 ${run('test:clean')}`,
-    'test:clean': () => 'tape \'{packages,codemods}/*/test/*.js\' \'{packages,codemods}/*/lib/**/*.spec.js\'',
+    'test:clean': () => `tape '${dirs}/*/test/*.js' '${dirs}/*/lib/**/*.spec.js'`,
     'test:slow': () => 'FORCE_COLOR=3 lerna run test',
     'coverage': () => `FORCE_COLOR=3 nyc ${run('test:clean')}`,
     'coverage:slow': () => 'FORCE_COLOR=3 lerna run coverage',
     'lint:slow': () => 'FORCE_COLOR=3 lerna run --no-bail lint',
     'lint:dot': () => 'putout .madrun.js',
     'lint-all': () => `MADRUN_NAME=1 ${run('lint:*')}`,
-    'lint': () => `putout .madrun.js {codemods,packages}/*/{bin,lib,test,*.js,.*.js} -f progress`,
+    'lint': () => `putout .madrun.js ${dirs}/*/{bin,lib,test,*.js,.*.js} -f progress`,
     'fix:lint': () => run('lint', '--fix'),
     'fix:lint:slow': () => 'lerna run --no-bail fix:lint',
     'bootstrap': () => 'lerna bootstrap',
     'report': () => `nyc report --reporter=text-lcov | coveralls`,
 };
 
+function getDirs(workspaces) {
+    const dirs = workspaces.map(cutStar);
+    return `{${dirs.join(',')}}`;
+}
