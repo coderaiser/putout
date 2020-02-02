c9c8ee27f8f585c2cf699a3422ca67c60dd97c1b
coderaiser <coderaiser@cloudcmd.io>
2020-02-02 14:08:38 +0200

    test(putout) parse-options: readCodemods: no .putout directory: coverage

--- packages/putout/lib/parse-options/index.spec.js
+++ packages/putout/lib/parse-options/index.spec.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const {join} = require('path');
 
 const test = require('supertape');
@@ -155,54 +156,103 @@ test('putout: parseOptions: custom options more important then default match', (
     t.end();
 });
 
-test(
-    'putout: parseOptions: custom match more important then custom options',
-    (t) => {
-        const empty = {};
-        
-        const readCodeMods = stub().returns([__dirname, empty]);
-        const readOptions = stub().returns([__dirname, empty]);
-        const readHomeOptions = stub().returns(empty);
-        
-        mockRequire('../../putout.json', empty);
-        
-        const parseOptions = reRequire('.');
-        
-        const options = {
-            rules: {
-                'remove-only': 'off',
-            },
-            match: {
-                'spec.js$': {
-                    'remove-only': 'on',
-                },
-            },
-        };
-        
-        const result = parseOptions({
-            name: 'parse-options.spec.js',
-            options,
-            readOptions,
-            readHomeOptions,
-            readCodeMods,
-        });
-        
-        const expected = {
-            dir: __dirname,
-            match: {
-                'spec.js$': {
-                    'remove-only': 'on',
-                },
-            },
-            rules: {
+test('putout: parseOptions: custom match more important then custom options', (t) => {
+    const empty = {};
+    
+    const readCodeMods = stub().returns([__dirname, empty]);
+    const readOptions = stub().returns([__dirname, empty]);
+    const readHomeOptions = stub().returns(empty);
+    
+    mockRequire('../../putout.json', empty);
+    
+    const parseOptions = reRequire('.');
+    
+    const options = {
+        rules: {
+            'remove-only': 'off',
+        },
+        match: {
+            'spec.js$': {
                 'remove-only': 'on',
             },
-        };
-        
-        stopAll();
-        
-        t.deepEqual(result, expected);
-        t.end();
-    },
-);
+        },
+    };
+    
+    const result = parseOptions({
+        name: 'parse-options.spec.js',
+        options,
+        readOptions,
+        readHomeOptions,
+        readCodeMods,
+    });
+    
+    const expected = {
+        dir: __dirname,
+        match: {
+            'spec.js$': {
+                'remove-only': 'on',
+            },
+        },
+        rules: {
+            'remove-only': 'on',
+        },
+    };
+    
+    stopAll();
+    
+    t.deepEqual(result, expected);
+    t.end();
+});
+
+test('putout: parseOptions: no code mods directory: .putout', (t) => {
+    const empty = {};
+    
+    const readOptions = stub().returns([__dirname, empty]);
+    const readHomeOptions = stub().returns(empty);
+    
+    mockRequire('../../putout.json', empty);
+    
+    const {readdirSync} = fs;
+    fs.readdirSync = () => {
+        throw 'error';
+    };
+    
+    const parseOptions = reRequire('.');
+    
+    const options = {
+        rules: {
+            'remove-only': 'off',
+        },
+        match: {
+            'spec.js$': {
+                'remove-only': 'on',
+            },
+        },
+    };
+    
+    const result = parseOptions({
+        name: 'parse-options.spec.js',
+        options,
+        readOptions,
+        readHomeOptions,
+    });
+    
+    const expected = {
+        dir: __dirname,
+        match: {
+            'spec.js$': {
+                'remove-only': 'on',
+            },
+        },
+        rules: {
+            'remove-only': 'on',
+        },
+    };
+    
+    stopAll();
+    fs.readdirSync = readdirSync;
+    
+    t.deepEqual(result, expected);
+    t.end();
+});
 
