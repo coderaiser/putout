e087df01fb1d5c31af5e4a6df5d6fa5273acb00f
coderaiser <coderaiser@cloudcmd.io>
2020-02-06 14:24:10 +0200

    fix(@putout/engine-parser) custom-parser: rm unused isObject check

--- packages/engine-parser/lib/custom-parser.js
+++ packages/engine-parser/lib/custom-parser.js
@@ -6,8 +6,6 @@ const espree = require('./parsers/espree');
 const esprima = require('./parsers/esprima');
 const tenko = require('./parsers/tenko');
 
-const isObject = (a) => typeof a === 'object';
-
 module.exports = (source, {parser, isTS, isFlow, isJSX}) => {
     if (parser === 'babel')
         return babel.parse(source, {
@@ -15,9 +13,6 @@ module.exports = (source, {parser, isTS, isFlow, isJSX}) => {
             isFlow,
             isJSX,
         });
-    
-    if (isObject(parser))
-        return parser.parse(source);
     
     if (parser === 'espree')
         return espree.parse(source);
