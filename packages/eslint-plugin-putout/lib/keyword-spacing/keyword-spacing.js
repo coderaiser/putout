87e760df6dca088e60e4db33afce501782b4aef1
coderaiser <coderaiser@cloudcmd.io>
2020-03-09 14:05:13 +0200

    refactor(eslint-plugin-putout) keyword-spacing

--- packages/eslint-plugin-putout/lib/keyword-spacing/keyword-spacing.js
+++ packages/eslint-plugin-putout/lib/keyword-spacing/keyword-spacing.js
@@ -2,25 +2,39 @@
 
 module.exports.report = () => 'Use spaces around "catch"';
 
-module.exports.fix = ({text}) => {
-    return text
-        .replace(/catch{/g, 'catch {')
-        .replace(/}catch/g, '} catch')
-        .replace(/catch \(/g, 'catch(');
+module.exports.fix = ({node, text}) => {
+    const {type} = node;
+    
+    if (type === 'TryStatement')
+        return fixCatch(text);
+    
+    return text;
 };
 
 module.exports.include = () => [
     'TryStatement',
 ];
 
-module.exports.filter = ({text}) => {
+module.exports.filter = ({node, text}) => {
+    const {type} = node;
+    
+    if (type === 'TryStatement')
+        return checkCatch(text);
+    
+    return false;
+};
+
+function checkCatch(text) {
     const before = text.includes('}catch');
     const after = text.includes('catch (');
     const afterOptional = text.includes('catch{');
     
-    if (!before && !after && !afterOptional)
-        return false;
-    
-    return true;
-};
+    return before || after || afterOptional;
+}
 
+function fixCatch(text) {
+    return text
+        .replace(/catch{/g, 'catch {')
+        .replace(/}catch/g, '} catch')
+        .replace(/catch \(/g, 'catch(');
+}
