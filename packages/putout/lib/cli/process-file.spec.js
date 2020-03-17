a2b884f4cbb4faf88d5603aa226b32ae711e4cd4
coderaiser <coderaiser@cloudcmd.io>
2020-03-17 12:51:42 +0200

    test(putout) cli: process-file: coverage

--- packages/putout/lib/cli/process-file.spec.js
+++ packages/putout/lib/cli/process-file.spec.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const fs = require('fs');
+
 const test = require('supertape');
 const mockRequire = require('mock-require');
 const stub = require('@cloudcmd/stub');
 
 const {reRequire, stopAll} = mockRequire;
 
-test('putout: process-files: get formatter', (t) => {
+test('putout: cli: process-file: get formatter', (t) => {
     const formatter = stub();
     mockRequire('putout-formatter-xxx', formatter);
     
@@ -19,13 +21,62 @@ test('putout: process-files: get formatter', (t) => {
     t.end();
 });
 
-test('putout: process-files: get formatter: exit', (t) => {
+test('putout: cli: process-file: get formatter: exit', (t) => {
     const exit = stub();
     
     const {_getFormatter} = reRequire('./process-file');
     _getFormatter('xxx', exit);
     
     t.ok(exit.called);
+    t.end();
+});
+
+test('putout: cli: process-file: eslint', (t) => {
+    const eslint = stub().returns(['', []]);
+    const {readFileSync} = fs;
+    
+    const code = 'log123("hello")';
+    const fix = false;
+    const name = 'example.js';
+    const log = stub();
+    const ruler = {};
+    const write = stub();
+    const fileCache = {
+        canUseCache: stub().returns(false),
+        setInfo: stub(),
+    };
+    
+    mockRequire('../eslint', eslint);
+    fs.readFileSync = (name, options) => {
+        if (name === 'example.js')
+            return code;
+        
+        return readFileSync(name, options);
+    };
+    
+    const processFile = reRequire('./process-file');
+    const fn = processFile({
+        fix,
+        log,
+        ruler,
+        write,
+        fileCache,
+    });
+    
+    fn('example.js', 0, {
+        length: 1,
+    });
+    
+    stopAll();
+    fs.readFileSync = readFileSync;
+    
+    const expected = {
+        code,
+        fix,
+        name,
+    };
+    
+    t.ok(eslint.calledWith(expected), 'should call eslint');
     t.end();
 });
 
