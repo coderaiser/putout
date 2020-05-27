048f976503905ec9993ff1770b0771d617a7c08e
coderaiser <coderaiser@cloudcmd.io>
2020-05-27 15:20:58 +0300

    test(@putout/compare) coverage

--- packages/compare/lib/is.spec.js
+++ packages/compare/lib/is.spec.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const test = require('supertape');
-const {parseTemplate} = require('./is');
+const {
+    parseTemplate,
+    isNameStr,
+    isObjectStr,
+    isArrayStr,
+    isAnyStr,
+} = require('./is');
 
 test('compare: is: parseTemplate: program', (t) => {
     const [node] = parseTemplate('const t = "hi"', {
@@ -9,6 +15,48 @@ test('compare: is: parseTemplate: program', (t) => {
     });
     
     t.equal(node.type, 'Program');
+    t.end();
+});
+
+test('compare: is: isNameStr: no', (t) => {
+    const result = isNameStr('hello');
+    
+    t.notOk(result);
+    t.end();
+});
+
+test('compare: is: isNameStr: yes', (t) => {
+    const result = isNameStr('__a');
+    
+    t.ok(result);
+    t.end();
+});
+
+test('compare: is: isObjectStr: no', (t) => {
+    const result = isObjectStr('hello');
+    
+    t.notOk(result);
+    t.end();
+});
+
+test('compare: is: isObjectStr', (t) => {
+    const result = isObjectStr('__object');
+    
+    t.ok(result);
+    t.end();
+});
+
+test('compare: is: isArrayStr', (t) => {
+    const result = isArrayStr('__array');
+    
+    t.ok(result);
+    t.end();
+});
+
+test('compare: is: isAnyStr', (t) => {
+    const result = isAnyStr('__');
+    
+    t.ok(result);
     t.end();
 });
 
