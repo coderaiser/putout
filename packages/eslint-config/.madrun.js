8c26aa68c393ea6fe2c358792683667757675016
coderaiser <coderaiser@cloudcmd.io>
2020-06-09 19:26:42 +0300

    feature(@putout/eslint-cofig) .eslintrc.json -> eslintrc.json

--- packages/eslint-config/.madrun.js
+++ packages/eslint-config/.madrun.js
@@ -3,7 +3,7 @@
 const {run} = require('madrun');
 
 module.exports = {
-    'lint:json': () => 'jsonlint -q .eslintrc.json',
+    'lint:json': () => 'jsonlint -q eslintrc.json',
     'lint:js': () => 'putout .madrun.js',
     'lint': () => run('lint:*'),
     'fix:lint': () => run('lint', '--fix'),
