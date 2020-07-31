379de9c3a8857caf3764b923253468261e8d789f
coderaiser <coderaiser@cloudcmd.io>
2020-07-31 11:42:19 +0300

    fix(@putout/engine-loader) babel plugins: rm useless options

--- packages/engine-loader/lib/transforms/babel.js
+++ packages/engine-loader/lib/transforms/babel.js
@@ -4,8 +4,6 @@ const {transformFromAstSync} = require('@babel/core');
 
 module.exports = (ast, code, name) => {
     transformFromAstSync(ast, code, {
-        ast: true,
-        code: false,
         cloneInputAst: false,
         plugins: [
             name,
