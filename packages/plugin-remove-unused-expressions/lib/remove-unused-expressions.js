1bc853173b83cdc0879a9228cc97ea05128b0d25
coderaiser <coderaiser@cloudcmd.io>
2020-09-28 18:54:14 +0300

    feature(@putout/plugin-remove-unused-expressions) speed up: find -> traverse

--- packages/plugin-remove-unused-expressions/lib/remove-unused-expressions.js
+++ packages/plugin-remove-unused-expressions/lib/remove-unused-expressions.js
@@ -2,10 +2,19 @@
 
 module.exports.report = () => 'Unused expression statement';
 
-module.exports.find = (ast, {push, traverse}) => {
-    const strictModeNodes = [];
-    
-    traverse(ast, {
+module.exports.fix = (path) => {
+    path.remove();
+};
+
+module.exports.traverse = ({push, listStore}) => {
+    return {
+        Program: {
+            exit() {
+                listStore()
+                    .slice(1)
+                    .map(push);
+            },
+        },
         ExpressionStatement(path) {
             const expressionPath = path.get('expression');
             
@@ -24,21 +33,12 @@ module.exports.find = (ast, {push, traverse}) => {
             const {node} = expressionPath;
             
             if (node.value === 'use strict') {
-                strictModeNodes.push(expressionPath);
+                listStore(expressionPath);
                 return;
             }
             
             push(expressionPath);
         },
-    });
-    
-    if (strictModeNodes.length === 1)
-        return;
-    
-    return strictModeNodes.slice(1);
-};
-
-module.exports.fix = (path) => {
-    path.remove();
+    };
 };
 
