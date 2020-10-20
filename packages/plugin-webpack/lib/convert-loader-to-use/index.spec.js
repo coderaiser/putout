0fc4757406c8477f8f364e4b76b6384f242c5ccd
coderaiser <coderaiser@cloudcmd.io>
2020-10-20 10:33:14 +0300

    test(@putout/plugin-webpack) putout -> webpack

--- packages/plugin-webpack/lib/convert-loader-to-use/index.spec.js
+++ packages/plugin-webpack/lib/convert-loader-to-use/index.spec.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const test = require('@putout/test')(__dirname, {
-    'putout/convert-loader-to-use': require('.'),
+    'webpack/convert-loader-to-use': require('.'),
 });
 
-test('plugin-putout: report', (t) => {
+test('plugin-webpack: convert-loader-to-use: report', (t) => {
     t.report('loader', `"use" should be used instead of exclamation mark in loaders`);
     t.end();
 });
 
-test('plugin-putout: transform', (t) => {
+test('plugin-webpack: convert-loader-to-use: transform', (t) => {
     t.transform('loader');
     t.end();
 });
 
-test('plugin-putout: no transform: query', (t) => {
+test('plugin-webpack: convert-loader-to-use: no transform: query', (t) => {
     t.noTransform('query');
     t.end();
 });
--- packages/plugin-webpack/lib/convert-query-loader-to-use/index.spec.js
+++ packages/plugin-webpack/lib/convert-query-loader-to-use/index.spec.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const test = require('@putout/test')(__dirname, {
-    'putout/convert-query-loader-to-use': require('.'),
+    'webpack/convert-query-loader-to-use': require('.'),
 });
 
-test('plugin-putout: report', (t) => {
+test('plugin-webpack: convert-query-loader-to-use: report', (t) => {
     t.report('query', `"use" should be used instead of query in loaders`);
     t.end();
 });
 
-test('plugin-putout: transform', (t) => {
+test('plugin-webpack: convert-query-loader-to-use: transform', (t) => {
     t.transform('query');
     t.end();
 });
