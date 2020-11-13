4b33ee9583babf7856ba7d1554ac1926c30104ad
coderaiser <coderaiser@cloudcmd.io>
2020-11-13 17:37:57 +0200

    feature(@putout/processor-json) add

---
+++ packages/processor-json/lib/json.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const prefix = '__putout_processor_json(';
+const sufix = ');';
+
+module.exports.extensions = [
+    'json',
+];
+
+module.exports.preProcess = (rawSource) => {
+    const source = `${prefix}${rawSource}${sufix}`;
+    return [{
+        startLine: 0,
+        source,
+    }];
+};
+
+module.exports.postProcess = (rawSource, list) => {
+    const [source] = list;
+    const length = source.length - sufix.length;
+    
+    return source.slice(prefix.length, length);
+};
+
---
+++ packages/processor-json/test/json.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const {join} = require('path');
+const {readFile} = require('fs').promises;
+
+const test = require('supertape');
+const {runProcessors} = require('@putout/engine-processor');
+const processFile = require('putout/lib/cli/process-file');
+
+const process = getProcess({
+    extension: 'json',
+    processors: [
+        'json',
+    ],
+    plugins: [
+        'eslint',
+    ],
+});
+
+test('putout: processor: json', async (t) => {
+    const {
+        output,
+        processedSource,
+    } = await process('eslintrc');
+    
+    t.equal(processedSource, output);
+    t.end();
+});
+
+function getProcess({processors, plugins, extension}) {
+    return async (name) => {
+        const inputName = join(__dirname, 'fixture', `${name}.${extension}`);
+        const outputName = join(__dirname, 'fixture', `${name}-fix.${extension}`);
+        
+        const rawSource = await readFile(inputName, 'utf8');
+        const output = await readFile(outputName, 'utf8');
+        const options = {
+            dir: __dirname,
+            processors,
+            plugins,
+        };
+        const index = 0;
+        const length = 1;
+        
+        const process = processFile({
+            fix: true,
+        });
+        
+        const {processedSource} = await runProcessors({
+            name: inputName,
+            process,
+            options,
+            rawSource,
+            index,
+            length,
+        });
+        
+        return {
+            processedSource,
+            output,
+        };
+    };
+}
+
