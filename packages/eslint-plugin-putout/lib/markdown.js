92583da976c9baea14a698d31bdc29e8abb6c686
coderaiser <coderaiser@cloudcmd.io>
2020-11-18 15:02:40 +0200

    feature(eslint-plugin-putout) markdown: add support of typescript

--- packages/eslint-plugin-putout/lib/markdown.js
+++ packages/eslint-plugin-putout/lib/markdown.js
@@ -1,19 +1,31 @@
 'use strict';
 
+const commonRules = {
+    'no-undef': 'off',
+    'no-multi-spaces': 'off',
+    'no-empty': 'off',
+    'eol-last': 'off',
+    'quotes': 'off',
+    'node/no-extraneous-require': 'off',
+    'node/no-unpublished-require': 'off',
+    'node/no-missing-require': 'off',
+    'node/no-missing-import': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+    'node/no-unsupported-features/node-builtins': 'off',
+};
+
 module.exports = [{
     files: '*.md{js}',
-    rules: {
-        'no-undef': 'off',
-        'no-multi-spaces': 'off',
-        'no-empty': 'off',
-        'eol-last': 'off',
-        'quotes': 'off',
-        'node/no-extraneous-require': 'off',
-        'node/no-unpublished-require': 'off',
-        'node/no-missing-require': 'off',
-        'node/no-missing-import': 'off',
-        'node/no-unsupported-features/es-syntax': 'off',
-        'node/no-unsupported-features/node-builtins': 'off',
+    rules: commonRules,
+}, {
+    files: '*.md{ts}',
+    rules: commonRules,
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+            plugins: ['@babel/plugin-syntax-typescript'],
+        },
     },
 }];
 
