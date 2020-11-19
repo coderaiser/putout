62d9e1775b516a23345aa1050f7016cfc27d6dc5
coderaiser <coderaiser@cloudcmd.io>
2020-11-19 19:24:55 +0200

    chore(cut-legacy) lint: add *.md

--- codemods/cut-legacy/.madrun.js
+++ codemods/cut-legacy/.madrun.js
@@ -5,7 +5,7 @@ const {run} = require('madrun');
 module.exports = {
     'test': () => `tape 'test/*.js'`,
     'watch:test': () => `nodemon -w lib -w test -x ${run('test')}`,
-    'lint': () => 'putout lib test .madrun.js',
+    'lint': () => 'putout lib test *.js *.json *.md',
     'fix:lint': () => run('lint', '--fix'),
     'coverage': () => `nyc ${run('test')}`,
     'report': () => `nyc report --reporter=text-lcov | coveralls || true`,
