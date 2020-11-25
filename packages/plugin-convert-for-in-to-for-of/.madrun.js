96db0fd612bc0560577ce74c0507e87fd4ab153d
coderaiser <coderaiser@cloudcmd.io>
2020-11-25 21:14:37 +0200

    chore(@putout/plugin-convert-in-to-for-of) lint

--- packages/plugin-convert-for-in-to-for-of/.madrun.js
+++ packages/plugin-convert-for-in-to-for-of/.madrun.js
@@ -1,24 +1,11 @@
 'use strict';
 
-const {
-    run,
-    predefined,
-} = require('madrun');
-
-const {putout} = predefined;
+const {run} = require('madrun');
 
 module.exports = {
     'test': () => `tape 'test/*.js' 'lib/**/*.spec.js'`,
     'watch:test': () => `nodemon -w lib -w test -x ${run('test')}`,
-    
-    'lint': () => putout({
-        names: [
-            'lib',
-            'test',
-            '.madrun.js',
-        ],
-    }),
-    
+    'lint': () => 'putout .',
     'fix:lint': () => run('lint', '--fix'),
     'coverage': () => `nyc ${run('test')}`,
     'report': () => `nyc report --reporter=text-lcov | coveralls || true`,
