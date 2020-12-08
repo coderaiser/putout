9969d84a34436faf70360a048103ecc1dfa51c03
coderaiser <coderaiser@cloudcmd.io>
2020-12-08 19:02:21 +0200

    feature(@putout/engine-loader) simplify plugins/processors search

--- packages/engine-loader/lib/load.js
+++ packages/engine-loader/lib/load.js
@@ -1,27 +1,29 @@
 'use strict';
 
-const tryCatch = require('try-catch');
-
 const wrapPlugin = require('./wrap-plugin');
 const getModulePath = require('./get-module-path');
 
 const bigFirst = (a) => `${a[0].toUpperCase()}${a.slice(1)}`;
 
+function getPath(namespace, type, name) {
+    let path = getModulePath(`@${namespace}/${type}-${name}`);
+    
+    if (!path)
+        path = getModulePath(`${namespace}-${type}-${name}`);
+    
+    return path;
+}
+
 const load = (type) => ({name, namespace}) => {
     if (namespace !== 'putout')
         return wrapPlugin(name, namespace);
     
-    const [, npmPlugin] = tryCatch(require, getModulePath(`@${namespace}/${type}-${name}`));
+    const pluginPath = getPath(namespace, type, name);
     
-    if (npmPlugin)
-        return npmPlugin;
+    if (!pluginPath)
+        throw Error(`${bigFirst(type)} "${namespace}-${type}-${name}" could not be found!`);
     
-    const [, userPlugin] = tryCatch(require, getModulePath(`${namespace}-${type}-${name}`));
-    
-    if (userPlugin)
-        return userPlugin;
-    
-    throw Error(`${bigFirst(type)} "${namespace}-${type}-${name}" could not be found!`);
+    return require(pluginPath);
 };
 
 module.exports.loadPlugin = load('plugin');
