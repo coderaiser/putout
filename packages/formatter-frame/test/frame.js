d54150b97b9bbf2cdd69dae518e458b22e939c35
coderaiser <coderaiser@cloudcmd.io>
2020-12-16 00:55:58 +0200

    test(@putout/formatter-frame) progress -> frame

--- packages/formatter-frame/test/frame.js
+++ packages/formatter-frame/test/frame.js
@@ -6,17 +6,17 @@ const test = require('@putout/test')(__dirname, {
     'remove-unused-variables': require('@putout/plugin-remove-unused-variables'),
 });
 
-test('formatter: progress', (t) => {
+test('formatter: frame', (t) => {
     t.format(progress, 'var');
     t.end();
 });
 
-test('formatter: progress: no', (t) => {
+test('formatter: frame: no', (t) => {
     t.format(progress, 'no');
     t.end();
 });
 
-test('formatter: progress: many', (t) => {
+test('formatter: frame: many', (t) => {
     t.formatMany(progress, ['var', 'var']);
     t.end();
 });
