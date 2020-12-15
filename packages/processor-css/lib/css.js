e77961234b6ec21452c05e932c0688692a39f27f
coderaiser <coderaiser@cloudcmd.io>
2020-12-15 17:23:24 +0200

    feature(@putout/processor-css) add ability to find config

--- packages/processor-css/lib/css.js
+++ packages/processor-css/lib/css.js
@@ -1,20 +1,30 @@
 'use strict';
 
+const {cosmiconfig} = require('cosmiconfig');
 const stylelint = require('stylelint');
+const once = require('once');
 const config = require('../stylelintrc');
-
-const configBasedir = process.cwd();
 
 module.exports.files = [
     '*.css',
 ];
 
+const loadConfig = once(async () => {
+    const explorer = cosmiconfig('stylelint');
+    const result = await explorer.search();
+    
+    return {
+        ...config,
+        ...result && result.config,
+    };
+});
+
 module.exports.process = async (code, {fix}) => {
+    const config = await loadConfig();
     const {output, results} = await stylelint.lint({
         fix,
         code,
         config,
-        configBasedir,
     });
     
     const {warnings} = results[0];
--- packages/processor-css/test/css.js
+++ packages/processor-css/test/css.js
@@ -4,8 +4,13 @@ const {join} = require('path');
 const {readFile} = require('fs').promises;
 
 const test = require('supertape');
+const mockRequire = require('mock-require');
+
+const stub = require('@cloudcmd/stub');
 const {runProcessors} = require('@putout/engine-processor');
 const processFile = require('putout/lib/cli/process-file');
+
+const {stopAll, reRequire} = mockRequire;
 
 test('putout: processor: css', async (t) => {
     const name = 'style';
@@ -59,6 +64,64 @@ test('putout: processor: css: places', async (t) => {
     
     const expected = [{
         message: 'Expected indentation of 4 spaces (indentation)',
+        position: {
+            column: 1,
+            line: 2,
+        },
+        rule: 'indentation (stylelint)',
+    }];
+    
+    t.deepEqual(places, expected);
+    t.end();
+});
+
+test('putout: processor: css: found config', async (t) => {
+    const name = 'style';
+    const inputName = join(__dirname, 'fixture', `${name}.css`);
+    
+    const rawSource = await readFile(inputName, 'utf8');
+    const options = {
+        processors: [
+            'css',
+        ],
+    };
+    
+    const load = async () => {};
+    const search = stub().returns({
+        config: {
+            rules: {
+                indentation: 2,
+            },
+        },
+    });
+    
+    const cosmiconfig = stub().returns({
+        search,
+        load,
+    });
+    
+    mockRequire('cosmiconfig', {
+        cosmiconfig,
+    });
+    
+    reRequire('../lib/css');
+    const {runProcessors} = reRequire('@putout/engine-processor');
+    
+    const fix = false;
+    const {places} = await runProcessors({
+        fix,
+        name: inputName,
+        processFile: processFile({
+            fix,
+        }),
+        options,
+        rawSource,
+    });
+    
+    stopAll();
+    
+    const expected = [{
+        message: 'Expected indentation of 2 spaces (indentation)',
         position: {
             column: 1,
             line: 2,
