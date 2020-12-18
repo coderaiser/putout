708b499434690ff57af3511d761619878a7d3da0
coderaiser <coderaiser@cloudcmd.io>
2020-12-18 23:41:35 +0200

    feature(eslint-plugin-putout) markdown: disable node/no-unpublished-import

--- packages/eslint-plugin-putout/lib/markdown.js
+++ packages/eslint-plugin-putout/lib/markdown.js
@@ -11,6 +11,7 @@ const commonRules = {
     'node/no-extraneous-require': 'off',
     'node/no-extraneous-import': 'off',
     'node/no-unpublished-require': 'off',
+    'node/no-unpublished-import': 'off',
     'node/no-missing-require': 'off',
     'node/no-missing-import': 'off',
     'node/no-unsupported-features/es-syntax': 'off',
