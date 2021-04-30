0441bd768138b240eae0c14c3855d1092433e798
coderaiser <coderaiser@cloudcmd.io>
2021-04-30 13:09:57 +0300

    test(@putout/processor-javascript) apply @putout/test/processor

--- packages/processor-javascript/test/javascript.js
+++ packages/processor-javascript/test/javascript.js
@@ -1,37 +1,18 @@
 'use strict';
 
-const {join} = require('path');
-const {readFile} = require('fs/promises');
+const {createTest} = require('@putout/test/processor');
 
-const test = require('supertape');
-const {runProcessors} = require('@putout/engine-processor');
-const processFile = require('putout/process-file');
+const test = createTest(__dirname, {
+    extension: 'js',
+    processors: [
+        'javascript',
+    ],
+    plugins: [
+        'eslint',
+    ],
+});
 
 test('putout: processor: javascript', async (t) => {
-    const inputName = join(__dirname, 'fixture', 'simple.js');
-    const outputName = join(__dirname, 'fixture', 'simple-fix.js');
-    const rawSource = await readFile(inputName, 'utf8');
-    const output = await readFile(outputName, 'utf8');
-    const options = {
-        dir: __dirname,
-        processors: [
-            'javascript',
-        ],
-        plugins: [
-            'remove-unused-variables',
-        ],
-    };
-    
-    const fix = true;
-    const {processedSource} = await runProcessors({
-        fix,
-        name: inputName,
-        processFile: processFile({fix}),
-        options,
-        rawSource,
-    });
-    
-    t.equal(processedSource, output);
-    t.end();
+    await t.process('simple', ['remove-unused-variables']);
 });
 
