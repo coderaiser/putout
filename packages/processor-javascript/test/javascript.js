1b7d7dc2b72c8e1701505056e6ed42cb9d262580
coderaiser <mnemonic.enemy@gmail.com>
2021-05-01 20:12:46 +0300

    test(@putout/processor-javascript) rm useless plugins

--- packages/processor-javascript/test/javascript.js
+++ packages/processor-javascript/test/javascript.js
@@ -3,16 +3,12 @@
 const {createTest} = require('@putout/test/processor');
 
 const test = createTest(__dirname, {
-    extension: 'js',
     processors: [
         'javascript',
-    ],
-    plugins: [
-        'eslint',
     ],
 });
 
 test('putout: processor: javascript', async (t) => {
-    await t.process('simple', ['remove-unused-variables']);
+    await t.process('simple.js', ['remove-unused-variables']);
 });
 
