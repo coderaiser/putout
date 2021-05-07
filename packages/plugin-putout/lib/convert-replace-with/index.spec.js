7c55619b6335d0da028d972511e82885a536bd5b
coderaiser <coderaiser@cloudcmd.io>
2021-05-07 17:00:12 +0300

    test(@putout/plugin-putout) convert-replace-with: improve description

--- packages/plugin-putout/lib/convert-replace-with/index.spec.js
+++ packages/plugin-putout/lib/convert-replace-with/index.spec.js
@@ -4,32 +4,32 @@ const test = require('@putout/test')(__dirname, {
     'putout/convert-replace-with': require('.'),
 });
 
-test('plugin-putout: report', (t) => {
+test('plugin-putout: convert-replace-with: report', (t) => {
     t.report('replace-with', `"operator.replaceWith" should be called instead of "path.replaceWith"`);
     t.end();
 });
 
-test('plugin-putout: transform', (t) => {
+test('plugin-putout: convert-replace-with: transform', (t) => {
     t.transform('replace-with');
     t.end();
 });
 
-test('plugin-putout: transform: replaceWith exists', (t) => {
+test('plugin-putout: convert-replace-with: transform: replaceWith exists', (t) => {
     t.transform('replace-with-exists');
     t.end();
 });
 
-test('plugin-putout: transform: replaceWith multiple exists', (t) => {
+test('plugin-putout: convert-replace-with: transform: replaceWith multiple exists', (t) => {
     t.transform('replace-with-multiple-exists');
     t.end();
 });
 
-test('plugin-putout: transform: insertAfter exists', (t) => {
+test('plugin-putout: convert-replace-with: transform: insertAfter exists', (t) => {
     t.transform('insert-after-exists');
     t.end();
 });
 
-test('plugin-putout: transform: putout declared', (t) => {
+test('plugin-putout: convert-replace-with: transform: putout declared', (t) => {
     t.transform('putout-declared');
     t.end();
 });
