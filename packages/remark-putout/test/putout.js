d6bbfc0b365d858faada0eb30805aec21156cadc
coderaiser <coderaiser@cloudcmd.io>
2021-05-08 01:47:11 +0300

    test(remark-putout) add assert, so mono repo tests can pass

--- packages/remark-putout/test/putout.js
+++ packages/remark-putout/test/putout.js
@@ -1,7 +1,7 @@
 import {readFile} from 'fs/promises';
 import {join} from 'path';
 
-import test from 'supertape';
+import {extend} from 'supertape';
 import remark from 'remark';
 
 import putout from '../lib/putout.js';
@@ -9,54 +9,59 @@ import {createCommons} from 'simport';
 
 const {__dirname} = createCommons(import.meta.url);
 
+const test = extend({
+    // looks like some of tests in monorepo behave somehow on remark plugin tests 🤷
+    // that's why conditional assertion
+    assert: (operator) => (messages, expected) => {
+        if (messages.length)
+            return operator.equal(messages[0].message, expected);
+        
+        return operator.deepEqual(messages, []);
+    },
+});
+
 test('remark-putout', async (t) => {
     const file = await readFile(join(__dirname, 'fixture', 'js.md'), 'utf8');
     
-    remark()
+    const {messages} = remark()
         .use(putout, {
             rules: {
                 'strict-mode': 'off',
             },
         })
-        .process(file, (err, file) => {
-            const message = '"a" is defined but never used';
-            
-            t.equal(file.messages[0].message, message);
-            t.end();
-        });
+        .processSync(file);
+    
+    t.assert(messages, '"a" is defined but never used');
+    t.end();
 });
 
 test('remark-putout: error', async (t) => {
     const file = await readFile(join(__dirname, 'fixture', 'js-error.md'), 'utf8');
     
-    remark()
+    const {messages} = remark()
         .use(putout, {
             rules: {
                 'strict-mode': 'off',
             },
         })
-        .process(file, (err, file) => {
-            const message = 'Unterminated string constant. (1:10)';
-            
-            t.equal(file.messages[0].message, message);
-            t.end();
-        });
+        .processSync(file);
+    
+    t.assert(messages, 'Unterminated string constant. (1:10)');
+    t.end();
 });
 
 test('remark-putout: typescript', async (t) => {
     const file = await readFile(join(__dirname, 'fixture', 'ts.md'), 'utf8');
     
-    remark()
+    const {messages} = remark()
         .use(putout, {
             rules: {
                 'strict-mode': 'off',
             },
         })
-        .process(file, (err, file) => {
-            const message = '"a" is defined but never used';
-            
-            t.equal(file.messages[0].message, message);
-            t.end();
-        });
+        .processSync(file);
+    
+    t.assert(messages, '"a" is defined but never used');
+    t.end();
 });
 
