5fc07a9250312768e8f567dab734163fdebeff6c
coderaiser <coderaiser@cloudcmd.io>
2021-09-12 22:01:15 +0300

    feature(@putout/plugin-eslint) add covert-ide-to-safe

--- packages/plugin-eslint/test/eslint.js
+++ packages/plugin-eslint/test/eslint.js
@@ -4,13 +4,18 @@ const test = require('@putout/test')(__dirname, {
     eslint: require('..'),
 });
 
-test('putout: plugin-eslint: move-putout-to-end-in-extends: report', (t) => {
+test('putout: plugin-eslint: report: move-putout-to-end-in-extends', (t) => {
     t.report('json', '"putout" should be in the end of the "extends" list');
     t.end();
 });
 
-test('putout: plugin-eslint: move-putout-to-end-in-extends: transform', (t) => {
+test('putout: plugin-eslint: transform: move-putout-to-end-in-extends', (t) => {
     t.transform('json');
+    t.end();
+});
+
+test('putout: plugin-eslint: transform: convert-ide-to-safe', (t) => {
+    t.transform('convert-ide-to-safe');
     t.end();
 });
 
