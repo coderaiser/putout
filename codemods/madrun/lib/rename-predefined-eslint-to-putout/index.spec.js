0893980c704d50d48fee0c6e180d37e36a0b7521
coderaiser <coderaiser@cloudcmd.io>
2021-09-15 14:54:58 +0300

    test(@putout/codemod-madrun) get rid of duplicates

--- codemods/madrun/lib/rename-predefined-eslint-to-putout/index.spec.js
+++ codemods/madrun/lib/rename-predefined-eslint-to-putout/index.spec.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const test = require('@putout/test')(__dirname, {
-    'madrun/rename-predefined-esilnt-to-putout': require('.'),
+    'madrun/rename-predefined-eslint-to-putout': require('.'),
 });
 
-test('madrun: rename predefined: report', (t) => {
+test('madrun: rename-predefined-eslint-to-putout: report', (t) => {
     t.report('eslint', `"putout" should be used instead of "eslint", when predefined`);
     t.end();
 });
 
-test('madrun: rename predefined: transform: eslint', (t) => {
+test('madrun: rename-predefined-eslint-to-putout: transform: eslint', (t) => {
     t.transform('eslint');
     t.end();
 });
 
-test('madrun: rename predefined: no transform: putout', (t) => {
+test('madrun: rename-predefined-eslint-to-putout: no transform: putout', (t) => {
     t.noTransform('putout');
     t.end();
 });
 
-test('madrun: rename predefined: no transform: putout', (t) => {
+test('madrun: rename-predefined-eslint-to-putout: no transform: eslint-rulesdir', (t) => {
     t.noTransform('eslint-rulesdir');
     t.end();
 });
--- codemods/madrun/test/madrun.js
+++ codemods/madrun/test/madrun.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const test = require('@putout/test')(__dirname, {
-    madrun: require('..'),
+    'codemod-madrun': require('..'),
 });
 
-test('plugin-madrun: transform', (t) => {
+test('codemod-madrun: transform', (t) => {
     t.transform('add-madrun-to-lint');
     t.end();
 });
 
-test('plugin-madrun: transform: no module.exports', (t) => {
+test('codemod-madrun: transform: no module.exports', (t) => {
     t.noTransform('no-module-exports');
     t.end();
 });
 
-test('plugin-madrun: no transform: module.exports not object', (t) => {
+test('codemod-madrun: no transform: module.exports not object', (t) => {
     t.noTransform('module-exports-not-object');
     t.end();
 });
