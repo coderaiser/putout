2e37636880daa2fd81db0b308b65d71cc058d224
coderaiser <coderaiser@cloudcmd.io>
2021-09-16 13:51:18 +0300

    test(@putout/codemod-remove-madrun-from-gitignore) get rid of duplicates (#82)

--- codemods/remove-madrun-from-gitignore/test/remove-madrun-from-gitignore.js
+++ codemods/remove-madrun-from-gitignore/test/remove-madrun-from-gitignore.js
@@ -1,19 +1,19 @@
 'use strict';
 
 const test = require('@putout/test')(__dirname, {
-    'remove-legacy-from-gitignore': require('..'),
+    'remove-madrun-from-gitignore': require('..'),
 });
 
-test('plugin-gitignore: report', (t) => {
+test('codemod-remove-madrun-form-gitignore: report', (t) => {
     t.report('gitignore', 'legacy should be removed from .gitignore');
 });
 
-test('plugin-gitignore: transform', (t) => {
+test('codemod-remove-madrun-from-gitignore: transform', (t) => {
     t.transform('gitignore');
     t.end();
 });
 
-test('plugin-gitignore: no transform: present', (t) => {
+test('codemod-remove-madrun-from-gitignore: no transform: present', (t) => {
     t.noTransform('not-present');
     t.end();
 });
