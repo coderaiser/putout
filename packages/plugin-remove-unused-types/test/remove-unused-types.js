886a4588b613976d7bb0a5924d3c658db98e2d36
coderaiser <coderaiser@cloudcmd.io>
2021-09-20 17:24:24 +0300

    test(putout) get rid of duplicates (#82)

--- packages/plugin-remove-unused-types/test/remove-unused-types.js
+++ packages/plugin-remove-unused-types/test/remove-unused-types.js
@@ -1,50 +1,50 @@
 'use strict';
 
 const test = require('@putout/test')(__dirname, {
-    'remove-useless-types': require('..'),
+    'remove-unused-types': require('..'),
 });
 
-test('remove usless types: report', (t) => {
+test('remove unused types: report', (t) => {
     t.report('unused', '"Point" is defined but never used');
     t.end();
 });
 
-test('remove usless types: transform', (t) => {
+test('remove unused types: transform', (t) => {
     t.transform('unused', '\n\n');
     t.end();
 });
 
-test('remove usless types: no transform: used', (t) => {
+test('remove unused types: no transform: used', (t) => {
     t.noTransform('used');
     t.end();
 });
 
-test('remove usless types: no transform: not defined', (t) => {
+test('remove unused types: no transform: not defined', (t) => {
     t.noTransform('not-defined');
     t.end();
 });
 
-test('remove usless types: no transform: export', (t) => {
+test('remove unused types: no transform: export', (t) => {
     t.noTransform('export');
     t.end();
 });
 
-test('remove usless types: no transform: export-named', (t) => {
+test('remove unused types: no transform: export-named', (t) => {
     t.noTransform('export-named');
     t.end();
 });
 
-test('remove usless types: no transform: export default', (t) => {
+test('remove unused types: no transform: export default', (t) => {
     t.noTransform('export-default');
     t.end();
 });
 
-test('remove usless types: no transform: export default object', (t) => {
+test('remove unused types: no transform: export default object', (t) => {
     t.noTransform('export-default-object');
     t.end();
 });
 
-test('remove usless types: no transform: bubling', (t) => {
+test('remove unused types: no transform: bubling', (t) => {
     t.noTransform('bubling');
     t.end();
 });
--- packages/plugin-remove-useless-types/test/remove-useless-types.js
+++ packages/plugin-remove-useless-types/test/remove-useless-types.js
@@ -4,37 +4,37 @@ const test = require('@putout/test')(__dirname, {
     'remove-useless-types': require('..'),
 });
 
-test('remove usless types: report', (t) => {
+test('remove useless types: report', (t) => {
     t.report('types', 'Avoid useless type declaration');
     t.end();
 });
 
-test('remove usless types: transform', (t) => {
+test('remove useless types: transform', (t) => {
     t.transform('types');
     t.end();
 });
 
-test('remove usless types: transform: return', (t) => {
+test('remove useless types: transform: return', (t) => {
     t.transform('return');
     t.end();
 });
 
-test('remove usless types: transform: array', (t) => {
+test('remove useless types: transform: array', (t) => {
     t.transform('array');
     t.end();
 });
 
-test('remove usless types: no transform: generic', (t) => {
+test('remove useless types: no transform: generic', (t) => {
     t.noTransform('generic');
     t.end();
 });
 
-test('remove usless types: no transform: export', (t) => {
+test('remove useless types: no transform: export', (t) => {
     t.noTransform('export');
     t.end();
 });
 
-test('remove usless types: no transform: qualified name', (t) => {
+test('remove useless types: no transform: qualified name', (t) => {
     t.noTransform('qualified-name');
     t.end();
 });
