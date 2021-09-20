ff3f3924041b022b341612409aae3ba3e7719347
coderaiser <coderaiser@cloudcmd.io>
2021-09-20 13:07:28 +0300

    test(@putout/plugin-remove-useless-templates) get rid of duplicates (#82)

--- packages/plugin-remove-useless-templates/test/remove-useless-templates.js
+++ packages/plugin-remove-useless-templates/test/remove-useless-templates.js
@@ -4,12 +4,12 @@ const test = require('@putout/test')(__dirname, {
     'remove-useless-templates': require('..'),
 });
 
-test('plugin-remove-useless-arguments: report', (t) => {
+test('plugin-remove-useless-templates: report', (t) => {
     t.report('template', 'Template string with only one variable should not be used');
     t.end();
 });
 
-test('plugin-remove-useless-arguments: transform', (t) => {
+test('plugin-remove-useless-templates: transform', (t) => {
     t.transform('template');
     t.end();
 });
