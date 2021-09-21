43f2ddef17e11c504389d990664cb9b4c4fb63d4
coderaiser <coderaiser@cloudcmd.io>
2021-09-21 20:03:02 +0300

    test(@putout/plugin-convert-spread-to-array-from) get rid of duplicates (#82)

--- packages/plugin-convert-spread-to-array-from/test/convert-spread-to-array-from.js
+++ packages/plugin-convert-spread-to-array-from/test/convert-spread-to-array-from.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const test = require('@putout/test')(__dirname, {
-    'remove-useless-spread': require('..'),
+    'convert-spread-to-array-from': require('..'),
 });
 
-test('plugin-remove-useless-spread: report', (t) => {
+test('plugin-convert-spread-to-array-from: report', (t) => {
     t.report('spread', `Array.from should be used instead of array spread`);
     t.end();
 });
 
-test('plugin-remove-useless-spread: transform', (t) => {
+test('plugin-convert-spread-to-array-from: transform', (t) => {
     t.transform('spread');
     t.end();
 });
 
-test('plugin-remove-useless-spread: no transform: elements', (t) => {
+test('plugin-convert-spread-to-array-from: no transform: elements', (t) => {
     t.noTransform('elements');
     t.end();
 });
