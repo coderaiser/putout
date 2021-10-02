ea560106dfc6c2d0ba4e19b8fc0b44d2a475f5fa
coderaiser <coderaiser@cloudcmd.io>
2021-10-03 01:24:27 +0300

    test(eslint-plugin-putout) simplify

--- packages/eslint-plugin-putout/test/eslint.mjs
+++ packages/eslint-plugin-putout/test/eslint.mjs
@@ -15,40 +15,40 @@ const test = extend({
         const fixture = await readFile(`${full}-fix.js`, 'utf8');
         const fix = true;
         
-        const [source] = await eslint({name, code, fix});
+        const [source] = await eslint({
+            name: full,
+            code,
+            fix,
+        });
         
         return operator.equal(source, fixture);
     },
+    comparePlaces: (operator) => async (name, expected) => {
+        const full = join(fixtureDir, name);
+        const code = await readFile(`${full}.js`, 'utf8');
+        
+        const [, places] = await eslint({
+            name: full,
+            code,
+        });
+        
+        return operator.deepEqual(places, expected);
+    },
 });
 
-test('eslint-plugin-putout: no-resolve: places', async (t) => {
-    const name = join(fixtureDir, 'no-unresolved.js');
-    const code = await readFile(name, 'utf8');
-    
-    const [, places] = await eslint({name, code});
-    const expected = {
+test('eslint-plugin-putout: no-resolve: places', async ({comparePlaces}) => {
+    await comparePlaces('no-unresolved-message', [{
         message: 'Always add an extension to relative imports',
         position: {
             column: 1,
             line: 1,
         },
         rule: 'putout/no-unresolved (eslint)',
-    };
-    
-    t.deepEqual(places[0], expected);
-    t.end();
+    }]);
 });
 
-test('eslint-plugin-putout: no-resolve: fix', async (t) => {
-    const name = join(fixtureDir, 'no-unresolved');
-    const code = await readFile(`${name}.js`, 'utf8');
-    const fixture = await readFile(`${name}-fix.js`, 'utf8');
-    const fix = true;
-    
-    const [source] = await eslint({name, code, fix});
-    
-    t.equal(source, fixture);
-    t.end();
+test('eslint-plugin-putout: no-resolve: fix', async ({process}) => {
+    await process('no-unresolved');
 });
 
 test('eslint-plugin-putout: evaluate: fix', async ({process}) => {
