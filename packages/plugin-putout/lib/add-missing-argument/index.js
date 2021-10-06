c6440e978786a375e1079b306fd77c42075e5aa1
coderaiser <coderaiser@cloudcmd.io>
2021-10-06 14:59:49 +0300

    feature(@putout/plugin-putout) add-missing-argument: add compare

--- packages/plugin-putout/lib/add-missing-argument/index.js
+++ packages/plugin-putout/lib/add-missing-argument/index.js
@@ -1,38 +1,10 @@
 'use strict';
 
-const {
-    operator,
-    types,
-} = require('putout');
+const {operator} = require('putout');
+const {addArgument} = operator;
 
-const {
-    Identifier,
-    ObjectPattern,
-    ObjectProperty,
-} = types;
-
-const {compare} = operator;
-
-module.exports.report = () => 'Argument "comparePlaces" is missing';
-
-module.exports.fix = (path) => {
-    const id = Identifier('comparePlaces');
-    path.scope.block.params = [
-        ObjectPattern([ObjectProperty(id, id)]),
-    ];
-};
-
-module.exports.traverse = ({push}) => ({
-    ReferencedIdentifier(path) {
-        if (path.node.name !== 'comparePlaces')
-            return;
-        
-        if (path.scope.hasBinding('comparePlaces'))
-            return;
-        
-        if (!compare(path.scope.path.parentPath, 'test("__a", (__args) => __body)'))
-            return false;
-        
-        push(path);
-    },
+module.exports = addArgument({
+    comparePlaces: ['{comparePlaces}', 'test("__a", (__args) => __body)'],
+    compare: ['{compare}', 'test("__a", (__args) => __body)'],
 });
+
