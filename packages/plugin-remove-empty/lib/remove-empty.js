2e170f000e442bf9a27f623bba43a5317891238b
coderaiser <coderaiser@cloudcmd.io>
2022-01-10 12:15:19 +0200

    feature(@putout/plugin-remove-empty) simplify namings

--- packages/plugin-remove-empty/lib/remove-empty.js
+++ packages/plugin-remove-empty/lib/remove-empty.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports.rules = {
-    block: require(`./remove-empty-block`),
+    block: require(`./block`),
     pattern: require('@putout/plugin-remove-empty-pattern'),
-    argument: require('./remove-empty-argument'),
-    import: require('./remove-empty-import'),
+    argument: require('./argument'),
+    import: require('./import'),
 };
 
