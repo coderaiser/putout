85af83b7bd589e7fb163704d37eac146ba5d56b4
coderaiser <coderaiser@cloudcmd.io>
2022-01-14 12:35:04 +0200

    feature(@putout/test) get rid of simport

--- packages/test/lib/test.mjs
+++ packages/test/lib/test.mjs
@@ -1,9 +1,12 @@
-import {createCommons} from 'simport';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
 import create from './test.js';
 
 export default create;
 
 export const createTest = (url, plugins) => {
-    const {__dirname} = createCommons(url);
+    const __filename = fileURLToPath(url);
+    const __dirname = dirname(__filename);
+    
     return create(__dirname, plugins);
 };
