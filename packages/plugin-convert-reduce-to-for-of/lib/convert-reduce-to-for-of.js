f95147eadc570cbaadc5dc16b851d8ad9f38b15b
coderaiser <coderaiser@cloudcmd.io>
2022-05-05 12:48:17 +0300

    feature(@putout/plugin-convert-reduce-to-for-of) delete binding after rename

--- packages/plugin-convert-reduce-to-for-of/lib/convert-reduce-to-for-of.js
+++ packages/plugin-convert-reduce-to-for-of/lib/convert-reduce-to-for-of.js
@@ -62,5 +62,6 @@ module.exports.replace = () => ({
 function rename(path, from, to) {
     const fnScope = path.get('declarations.0.init.arguments.0').scope;
     fnScope.rename(from.name, to.name);
+    delete path.scope.bindings[from.name];
 }
 
