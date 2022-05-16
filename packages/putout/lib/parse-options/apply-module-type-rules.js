3d5f9d36231981ca90667b82ad7c64477c304723
coderaiser <coderaiser@cloudcmd.io>
2022-05-16 14:02:10 +0300

    feature(putout) parse-optios: apply-module-type-rules: simplify

--- packages/putout/lib/parse-options/apply-module-type-rules.js
+++ packages/putout/lib/parse-options/apply-module-type-rules.js
@@ -1,25 +1,37 @@
 'use strict';
 
 const {assign} = Object;
-const getRuleByType = (a) => a === 'module' ? 'convert-commonjs-to-esm' : 'convert-esm-to-commonjs';
+const merge = require('../merge');
 
 module.exports = ({type}, options) => {
-    const rule = getRuleByType(type);
-    const js = '*.js';
-    
-    assign(options, {
-        match: {
-            ...options.match,
-            [js]: {
-                ...options.match?.[js],
-                [rule]: 'on',
-            },
-            '.eslintrc.json': {
-                ...options.match?.['.eslintrc.json'],
-                'eslint': 'on',
-                'eslint/convert-require-to-import': type === 'module' ? 'on' : 'off',
-            },
-        },
-    });
+    const rules = type === 'module' ? esm() : commonjs();
+    assign(options, merge(options, rules));
 };
 
+function commonjs() {
+    return {
+        match: {
+            '*.js': {
+                'convert-esm-to-commonjs': 'on',
+            },
+            '.eslintrc.json': {
+                'eslint': 'on',
+                'eslint/convert-require-to-import': 'off',
+            },
+        },
+    };
+}
+
+function esm() {
+    return {
+        match: {
+            '*.js': {
+                'convert-commonjs-to-esm': 'on',
+            },
+            '.eslintrc.json': {
+                'eslint': 'on',
+                'eslint/convert-require-to-import': 'on',
+            },
+        },
+    };
+}
