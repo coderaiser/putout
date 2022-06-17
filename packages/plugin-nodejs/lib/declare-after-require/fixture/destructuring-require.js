ad9bdeed9f9c1a69b3de7a9765a7ca3152efdc17
coderaiser <coderaiser@cloudcmd.io>
2022-06-17 16:33:11 +0300

    feature(@putout/plugin-nodejs) declare-after-require: add support of ExportDeclaration

--- packages/plugin-nodejs/lib/declare-after-require/fixture/destructuring-require.js
+++ packages/plugin-nodejs/lib/declare-after-require/fixture/destructuring-require.js
@@ -4,4 +4,7 @@ const {
     createConfig,
 } = require(DIR + 'config');
 
+const configPath = require(DIR + 'config');
+
+module.exports.configPath = configPath;
 module.exports.createConfigManager = createConfig;
