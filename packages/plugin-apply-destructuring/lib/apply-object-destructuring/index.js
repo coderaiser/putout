243d3a8c40d134f729660a479bf204cdda5e8abe
coderaiser <coderaiser@cloudcmd.io>
2023-02-23 23:19:59 +0200

    feature: @putout/plugin-apply-destructuring: drop useless match

--- packages/plugin-apply-destructuring/lib/apply-object-destructuring/index.js
+++ packages/plugin-apply-destructuring/lib/apply-object-destructuring/index.js
@@ -2,12 +2,6 @@
 
 module.exports.report = () => 'Use object destructuring';
 
-module.exports.exclude = () => [
-    'const __a = __b.__a || __c.__d',
-    'const __a = __b || __c(__object)',
-    'let __a = __b.__a || __c.__d',
-];
-
 module.exports.replace = () => ({
     'const __a = __b.__a': 'const {__a} = __b',
     'let __a = __b.__a': 'let {__a} = __b',
