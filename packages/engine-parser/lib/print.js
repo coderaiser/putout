31203efaabd369188b48a53e67301ec26dc3962b
coderaiser <coderaiser@cloudcmd.io>
2023-03-04 00:19:47 +0200

    feature: @putout/engine-parser: try to use babel/generate, when recast produces broken code with additional brace (https://github.com/benjamn/recast/issues/1248)

--- packages/engine-parser/lib/print.js
+++ packages/engine-parser/lib/print.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const {print} = require('@putout/recast');
-
+const generate = require('@babel/generator').default;
 const fixStrictMode = (a) => a.replace(`\n\n\n'use strict'`, `\n\n'use strict'`);
 const btoa = (a) => Buffer.from(a, 'binary').toString('base64');
 const addSourceMap = (sourceMapName, {code, map}) => !sourceMapName ? code : `${code}\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,${btoa(stringify(map))}\n`;
-
 const {stringify} = JSON;
-
+const {assign} = Object;
 module.exports = (ast, options = {}) => {
     const {sourceMapName} = options;
     const printOptions = {
@@ -16,8 +15,17 @@ module.exports = (ast, options = {}) => {
         wrapColumn: Infinity,
         ...options,
     };
-    
     const printed = print(ast, printOptions);
+    
+    if (checkBrackets(printed.code))
+        assign(printed, {
+            code: generate(ast, {
+                indent: {
+                    style: '    ',
+                },
+            }).code,
+        });
+    
     const {map} = printed;
     const code = fixStrictMode(printed.code);
     
@@ -26,4 +34,26 @@ module.exports = (ast, options = {}) => {
         map,
     });
 };
-
+function checkBrackets(source) {
+    const brackets = [];
+    let i = source.length;
+    
+    if (!i)
+        return false;
+    
+    while (--i) {
+        const current = source[i];
+        
+        if (current === ')') {
+            brackets.push(current);
+            continue;
+        }
+        
+        if (current === '(') {
+            brackets.pop();
+            continue;
+        }
+    }
+    
+    return brackets.length;
+}
--- packages/engine-parser/lib/print.spec.js
+++ packages/engine-parser/lib/print.spec.js
@@ -1,9 +1,20 @@
 'use strict';
 
-const test = require('supertape');
+const mockRequire = require('mock-require');
+const {
+    test,
+    stub,
+} = require('supertape');
 const montag = require('montag');
-
 const putout = require('putout');
+const {
+    stopAll,
+    reRequire,
+} = mockRequire;
+const {
+    parse,
+    print,
+} = putout;
 
 test('putout: parser: print: long lines', (t) => {
     const source = montag`
@@ -11,7 +22,6 @@ test('putout: parser: print: long lines', (t) => {
             const empty = {};
         });
     `;
-    
     const expected = montag`
         import {test} from 'supertape';
         test('putout: parseOptions: code mods directory: .putout: exclude node_modules', (t) => {
@@ -19,14 +29,57 @@ test('putout: parser: print: long lines', (t) => {
             t.end();
         });
     `;
-    
     const {code} = putout(source, {
-        plugins: [
-            'tape',
-        ],
+        plugins: ['tape'],
     });
     
     t.equal(code, expected);
     t.end();
 });
 
+test('putout: parser: print: bad recast output', (t) => {
+    const source = montag`
+        export default () => {
+          return (
+            <h1>hello</h1>
+          );
+        }
+    `;
+    const invalid = montag`
+        export default () => {
+          return (
+            <h1>hello</h1>)
+          );
+        }
+    `;
+    const expected = montag`
+        export default (() => {
+          return <h1>hello</h1>;
+        });
+    `;
+    const recastPrint = stub().returns({
+        code: invalid,
+    });
+    
+    mockRequire('@putout/recast', {
+        print: recastPrint,
+    });
+    const print = reRequire('./print');
+    const ast = parse(source);
+    const result = print(ast);
+    
+    stopAll();
+    
+    t.equal(result, expected);
+    t.end();
+});
+
+test('putout: parser: print: empty', (t) => {
+    const source = '';
+    const ast = parse(source);
+    const result = print(ast);
+    const expected = '';
+    
+    t.equal(result, expected);
+    t.end();
+});
