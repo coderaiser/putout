b302a90669ea2045d232297156610ab8039907d5
coderaiser <coderaiser@cloudcmd.io>
2023-03-04 16:28:04 +0200

    chore: madrun: test: add rules

--- .madrun.mjs
+++ .madrun.mjs
@@ -32,7 +32,7 @@ const putoutOffEnv = {
 };
 
 export default {
-    'test': () => [env, `tape '${dirs}/*/test/*.*' '${dirs}/*/{bin,lib}/**/*.spec.*'`],
+    'test': () => [env, `tape '${dirs}/*/test/*.*' '${dirs}/*/{bin,lib,rules}/**/*.spec.*'`],
     'test:inspect': () => [env, `node --inspect-brk --inspect=0.0.0.0 node_modules/.bin/${cutEnv('test')}`],
     'test:fail': async () => await run('test', '-f fail'),
     'test:slow': () => 'FORCE_COLOR=3 lerna run test',
