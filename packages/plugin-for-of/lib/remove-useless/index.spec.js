06627b0c6b0bce00b7a52bdfd875095eb97facd1
coderaiser <coderaiser@cloudcmd.io>
2023-03-16 11:25:41 +0200

    test: @putout/plugin-for-of: remove-useless

--- packages/plugin-for-of/lib/remove-useless/index.spec.js
+++ packages/plugin-for-of/lib/remove-useless/index.spec.js
@@ -4,45 +4,45 @@ const {createTest} = require('@putout/test');
 const removeUselessForOf = require('.');
 
 const test = createTest(__dirname, {
-    'remove-useless-for-of': removeUselessForOf,
+    'for-of/remove-useless': removeUselessForOf,
 });
 
-test('plugin-remove-useless-for-of: report', (t) => {
+test('plugin-for-of: remove-useless: report', (t) => {
     t.report('empty', 'Avoid useless for-of');
     t.end();
 });
 
-test('plugin-remove-useless-for-of: transform', (t) => {
+test('plugin-for-of: remove-useless: transform', (t) => {
     t.transform('empty', '\n');
     t.end();
 });
 
-test('plugin-remove-useless-for-of: transform: one', (t) => {
+test('plugin-for-of: remove-useless: transform: one', (t) => {
     t.transform('one');
     t.end();
 });
 
-test('plugin-remove-useless-for-of: no transform: many', (t) => {
+test('plugin-for-of: remove-useless: no transform: many', (t) => {
     t.noTransform('many');
     t.end();
 });
 
-test('plugin-remove-useless-for-of: no transform: not identifier', (t) => {
+test('plugin-for-of: remove-useless: no transform: not identifier', (t) => {
     t.noTransform('not-id');
     t.end();
 });
 
-test('plugin-remove-useless-for-of: no transform: refs', (t) => {
+test('plugin-for-of: remove-useless: no transform: refs', (t) => {
     t.noTransform('refs');
     t.end();
 });
 
-test('plugin-remove-useless-for-of: transform: no refs', (t) => {
+test('plugin-for-of: remove-useless: transform: no refs', (t) => {
     t.transform('no-refs');
     t.end();
 });
 
-test('plugin-remove-useless-for-of: transform: unused', (t) => {
+test('plugin-for-of: remove-useless: transform: unused', (t) => {
     t.transform('unused');
     t.end();
 });
