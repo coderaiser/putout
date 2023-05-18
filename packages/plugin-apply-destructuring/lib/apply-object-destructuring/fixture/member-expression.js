58fca3d155f0c1b32ec8c48110b74c5f840d15a5
coderaiser <coderaiser@cloudcmd.io>
2023-05-18 13:31:38 +0300

    chore: @putout/plugin-apply-destructuring: fixture

--- packages/plugin-apply-destructuring/lib/apply-object-destructuring/fixture/member-expression.js
+++ packages/plugin-apply-destructuring/lib/apply-object-destructuring/fixture/member-expression.js
@@ -8,7 +8,4 @@ const objectB = {
 
 const property = objectA.property || objectB.property;
 
-console.log(property);
-
-// prints "foo" (the value from objectB)
-
+console.log(property);// prints "foo" (the value from objectB)
