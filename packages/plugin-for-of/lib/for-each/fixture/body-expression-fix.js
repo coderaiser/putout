1cf7810c159ee1129f8bb9e0a4a13d9c161c90dc
coderaiser <coderaiser@cloudcmd.io>
2023-05-24 18:42:39 +0300

    chore: @putout/plugin-for-of: update fixtures

--- packages/plugin-for-of/lib/for-each/fixture/body-expression-fix.js
+++ packages/plugin-for-of/lib/for-each/fixture/body-expression-fix.js
@@ -2,11 +2,9 @@ let a = 0;
 const fn = currify(() => {
     for (const b of list)
         a += b;
-
 });
 
 const fn2 = currify(() => {
     for (const [i, b] of list.entries())
         a += i;
-
 });
