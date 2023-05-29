6312e790eebf3e0abd1eb8c45dfe5390a1950b1a
coderaiser <coderaiser@cloudcmd.io>
2023-05-29 13:07:30 +0300

    fix: putout: hang --staged (isomorphic-git/isomorphic-git#1768)

--- packages/putout/lib/cli/supported-files.js
+++ packages/putout/lib/cli/supported-files.js
@@ -4,21 +4,19 @@ const {normalize} = require('path');
 const picomatch = require('picomatch');
 
 let patterns = [];
+let isMatch;
 
 const rmDuplicates = (a) => Array.from(new Set(a));
 
 module.exports.add = (array) => {
     patterns = rmDuplicates(patterns.concat(array));
-};
-
-module.exports.isSupported = (name) => {
-    const isMatch = picomatch(patterns, {
+    isMatch = picomatch(patterns, {
         dot: true,
         matchBase: true,
     });
-    
-    return isMatch(name);
 };
+
+module.exports.isSupported = (name) => isMatch(name);
 
 module.exports.getSupportedGlob = (file) => normalize(`${file}/**/{${patterns.join(',')}}`);
 
