bc1de0ed0bdb2fa2c474a381c85c35754c180ed2
coderaiser <coderaiser@cloudcmd.io>
2023-05-31 17:20:08 +0300

    chore: @putout/plugin-promises: update fixture

--- packages/plugin-promises/lib/convert-reject-to-throw/index.spec.js
+++ packages/plugin-promises/lib/convert-reject-to-throw/index.spec.js
@@ -10,47 +10,47 @@ const test = createTest(__dirname, {
     ],
 });
 
-test('plugin-convert-reject-to-throw: transform: report', (t) => {
+test('plugin-promises: convert-reject-to-throw: transform: report', (t) => {
     t.report('reject', 'Reject is useless in async functions, use throw instead');
     t.end();
 });
 
-test('plugin-convert-reject-to-throw: transform', (t) => {
+test('plugin-promises: convert-reject-to-throw: transform', (t) => {
     t.transform('reject');
     t.end();
 });
 
-test('plugin-convert-reject-to-throw: transform: call', (t) => {
+test('plugin-promises: convert-reject-to-throw: transform: call', (t) => {
     t.transform('call');
     t.end();
 });
 
-test('plugin-convert-reject-to-throw: transform: await', (t) => {
+test('plugin-promises: convert-reject-to-throw: transform: await', (t) => {
     t.transform('await');
     t.end();
 });
 
-test('plugin-convert-reject-to-throw: no transform: not-async', (t) => {
+test('plugin-promises: convert-reject-to-throw: no transform: not-async', (t) => {
     t.noTransform('not-async');
     t.end();
 });
 
-test('plugin-convert-reject-to-throw: no report: not async', (t) => {
+test('plugin-promises: convert-reject-to-throw: no report: not async', (t) => {
     t.noReport('not-async');
     t.end();
 });
 
-test('plugin-convert-reject-to-throw: no transform: not-fn', (t) => {
+test('plugin-promises: convert-reject-to-throw: no transform: not-fn', (t) => {
     t.noTransform('not-fn');
     t.end();
 });
 
-test('plugin-convert-reject-to-throw: no transform: no arg', (t) => {
+test('plugin-promises: convert-reject-to-throw: no transform: no arg', (t) => {
     t.noTransform('no-arg');
     t.end();
 });
 
-test('plugin-convert-reject-to-throw: no report: no arg', (t) => {
+test('plugin-promises: convert-reject-to-throw: no report: no arg', (t) => {
     t.noReport('no-arg');
     t.end();
 });
