15f0333d5089c3cf235c0522090538525cfa6e90
coderaiser <coderaiser@cloudcmd.io>
2023-06-03 18:35:03 +0300

    chore: @putout/engine-parser: lint

--- packages/engine-parser/lib/parsers/babel/plugins.js
+++ packages/engine-parser/lib/parsers/babel/plugins.js
@@ -1,5 +1,16 @@
 'use strict';
 
-module.exports = ['importMeta', ['importAttributes', {
-    deprecatedAssertSyntax: true,
-}], 'dynamicImport', 'bigInt', 'classProperties', 'decorators-legacy', 'destructuringPrivate', 'exportDefaultFrom', 'throwExpressions', 'recordAndTuple'];
+module.exports = [
+    'importMeta',
+    ['importAttributes', {
+        deprecatedAssertSyntax: true,
+    }],
+    'dynamicImport',
+    'bigInt',
+    'classProperties',
+    'decorators-legacy',
+    'destructuringPrivate',
+    'exportDefaultFrom',
+    'throwExpressions',
+    'recordAndTuple',
+];
