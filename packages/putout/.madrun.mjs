5d24a9910cc4eefbcee14b92f0b98e781d594f9a
coderaiser <coderaiser@cloudcmd.io>
2023-06-06 17:02:52 +0300

    test: putout: coverage on node v20

--- packages/putout/.madrun.mjs
+++ packages/putout/.madrun.mjs
@@ -10,7 +10,8 @@ const testEnv = {
 
 export default {
     'wisdom': () => run(['lint', 'coverage']),
-    'test': () => [testEnv, `escover tape 'test/*.{js,mjs}' '{bin,lib}/**/*.spec.{js,mjs}'`],
+    'test': async () => [testEnv, `escover ${await cutEnv('test:raw')}`],
+    'test:raw': () => [testEnv, `tape 'test/*.{js,mjs}' '{bin,lib}/**/*.spec.{js,mjs}'`],
     'watch:test': async () => [testEnv, `nodemon -w bin -w lib -w test -x "${await cutEnv('test')} -f tap"`],
     'lint': () => `bin/putout.mjs .`,
     'fresh:lint': () => run('lint', '--fresh'),
@@ -18,7 +19,7 @@ export default {
     'fix:lint:fresh': () => run('fix:lint', '--fresh'),
     'lint:progress': () => run('lint', '--fix'),
     'lint:fresh': () => run('lint', '--fresh'),
-    'coverage': async () => [testEnv, `c8 ${await cutEnv('test')}`],
+    'coverage': async () => [testEnv, `c8 ${await cutEnv('test:raw')}`],
     'report': () => 'c8 report --reporter=lcov',
 };
 
