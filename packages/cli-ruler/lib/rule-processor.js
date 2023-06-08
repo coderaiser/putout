a629f49cac58bc6efa196a81b0b1a182431a233b
coderaiser <coderaiser@users.noreply.github.com>
2023-06-08 12:28:31 +0000

    chore: putout: actions: lint ☘️

--- packages/cli-ruler/lib/rule-processor.js
+++ packages/cli-ruler/lib/rule-processor.js
@@ -52,7 +52,10 @@ export function disable(rule, config) {
         return config;
     
     if (isTuple(config.rules[rule]))
-        config.rules[rule] = ['off', config.rules[rule][1]];
+        config.rules[rule] = [
+            'off',
+            config.rules[rule][1],
+        ];
     else
         config.rules[rule] = 'off';
     
