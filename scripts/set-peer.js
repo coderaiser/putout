71d19ba67233b554866ceaf5b9c540ca576e0207
coderaiser <coderaiser@cloudcmd.io>
2023-08-28 11:51:43 +0300

    chore: scripts: add updaters

---
+++ scripts/set-peer.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import {
+    readFile,
+    writeFile,
+} from 'node:fs/promises';
+
+const {parse, stringify} = JSON;
+
+console.log(process.cwd());
+
+const data = await readFile('./package.json', 'utf8');
+const json = parse(data);
+
+json.peerDependencies.putout = '>=31';
+
+await writeFile('./package.json', stringify(json, null, 2));
