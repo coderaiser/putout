300e044de780b87266953b46755782d0f7a27e13
coderaiser <coderaiser@cloudcmd.io>
2023-08-31 11:12:33 +0300

    test: @putout/engine-loader: load-plugins-async: coverage

--- packages/engine-loader/lib/plugins/load-plugins-async.spec.js
+++ packages/engine-loader/lib/plugins/load-plugins-async.spec.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const tryToCatch = require('try-to-catch');
 const test = require('supertape');
 const {loadPluginsAsync} = require('./load-plugins-async');
 
@@ -35,5 +36,14 @@ test('@putout/engine-loader: loadPluginsAsync: disabled: rule', async (t) => {
     const expected = [];
     
     t.deepEqual(result, expected);
+    t.end();
+});
+
+test('@putout/engine-loader: loadPluginsAsync: not found', async (t) => {
+    const [error] = await tryToCatch(loadPluginsAsync, {
+        pluginNames: ['hello'],
+    });
+    
+    t.equal(error.message, `Cannot find package 'putout-plugin-hello'`);
     t.end();
 });
