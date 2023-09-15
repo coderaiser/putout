d2188e54ed413a9c835d3a4a2987a24e923545ac
coderaiser <coderaiser@cloudcmd.io>
2023-09-15 11:11:34 +0300

    fix: @putout/plugin-apply-shorthand-properties: drop comment

--- packages/plugin-apply-shorthand-properties/lib/apply-shorthand-properties.js
+++ packages/plugin-apply-shorthand-properties/lib/apply-shorthand-properties.js
@@ -42,11 +42,6 @@ export const traverse = ({push, options}) => ({
             
             const {references, path} = bindingFrom;
             
-            // scope.rename doesn't handle AssignmentPattern
-            //if (path.isAssignmentPattern())
-            // scope.rename doesn't handle AssignmentPattern
-            //if (path.isAssignmentPattern())
-            //    continue;
             if (path.isImportDefaultSpecifier() || path.isImportSpecifier() || path.isObjectPattern() || path.get('id').isObjectPattern())
                 continue;
             
