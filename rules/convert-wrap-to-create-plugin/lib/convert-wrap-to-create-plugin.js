5b9a872e69c2f90fc20433d0fbba115538975134
coderaiser <coderaiser@cloudcmd.io>
2023-09-15 10:23:13 +0300

    feature: @putout/plugin-putout: drop support of ☘🐊 < 32

--- rules/convert-wrap-to-create-plugin/lib/convert-wrap-to-create-plugin.js
+++ rules/convert-wrap-to-create-plugin/lib/convert-wrap-to-create-plugin.js
@@ -1,8 +1,12 @@
+import {operator} from 'putout';
+
+const {rename} = operator;
+
 export const report = () => `Use 'createPlugin()' instead of 'wrap()'`;
 
 export const replace = () => ({
     'const wrap = require("../wrap")': (vars, path) => {
-        path.scope.rename('wrap', 'createPlugin');
+        rename(path, 'wrap', 'createPlugin');
         return 'const {createPlugin} = require("@putout/eslint/create-plugin")';
     },
 });
