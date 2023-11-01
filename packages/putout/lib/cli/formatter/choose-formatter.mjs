7acbdc5752b6e7dcfcbb0615c2cb99edd5547a96
coderaiser <coderaiser@cloudcmd.io>
2023-11-01 17:59:31 +0200

    feature: putout: cli: read userland config to search currently used formatter

--- packages/putout/lib/cli/formatter/choose-formatter.mjs
+++ packages/putout/lib/cli/formatter/choose-formatter.mjs
@@ -1,19 +1,36 @@
-import {choose as customChoose} from '@putout/cli-choose';
 import {readFile as customReadFile} from 'node:fs/promises';
 
+import {findUp} from 'find-up';
+import {choose as customChoose} from '@putout/cli-choose';
+
+const {isArray} = Array;
+const maybeFirst = (a) => isArray(a) ? a[0] : a;
 const {keys} = Object;
 const {parse} = JSON;
 const PREFIX = '@putout/formatter-';
 
-const infoPath = new URL('../../../package.json', import.meta.url).pathname;
-
 export const chooseFormatter = async ({readFile = customReadFile, choose = customChoose} = {}) => {
-    const data = await readFile(infoPath, 'utf8');
-    const parsed = parse(data);
+    const packageJson = await readJSON('package.json', {
+        readFile,
+    });
     
-    const formatters = getFormatters(parsed);
+    const formatters = getFormatters(packageJson);
     
-    return await choose('Choose formatter', formatters);
+    const {formatter} = await readJSON('putout.json', {
+        readFile,
+    });
+    
+    const {formatter: userFormatter} = await readJSON('.putout.json', {
+        up: true,
+        readFile,
+    });
+    
+    const currentFormatter = maybeFirst(userFormatter || formatter);
+    const index = formatters.indexOf(currentFormatter);
+    
+    return await choose('Choose formatter', formatters, {
+        autofocus: index >= 0 && index,
+    });
 };
 
 function getFormatters({dependencies}) {
@@ -28,4 +45,23 @@ function getFormatters({dependencies}) {
     }
     
     return formatters;
+}
+
+async function getPath(name, {up}) {
+    let path;
+    
+    if (up)
+        path = await findUp('.putout.json');
+    
+    if (!path)
+        path = new URL(`../../../${name}`, import.meta.url).pathname;
+    
+    return path;
+}
+
+async function readJSON(name, {up, readFile}) {
+    const path = await getPath(name, {up});
+    const json = await readFile(path, 'utf8');
+    
+    return parse(json);
 }
--- packages/putout/lib/cli/formatter/choose-formatter.spec.mjs
+++ packages/putout/lib/cli/formatter/choose-formatter.spec.mjs
@@ -33,13 +33,65 @@ test('putout: cli: choose-formatter: getFormatters', async (t) => {
     }));
     
     const choose = stub().returns('dump');
+    const autofocus = false;
+    
+    await chooseFormatter({
+        autofocus,
+        readFile,
+        choose,
+    });
+    
+    const expected = ['Choose formatter', ['dump'], {
+        autofocus: false,
+    }];
+    
+    t.calledWith(choose, expected);
+    t.end();
+});
+
+test('putout: cli: choose-formatter: getFormatters: autofocus', async (t) => {
+    const readFile = stub().returns(stringify({
+        formatter: ['progress-bar', {}],
+        dependencies: {
+            '@putout/formatter-dump': '^7.0.0',
+            '@putout/formatter-progress-bar': '^7.0.0',
+        },
+    }));
+    
+    const choose = stub().returns('progress-bar');
     
     await chooseFormatter({
         readFile,
         choose,
     });
     
-    const expected = ['Choose formatter', ['dump']];
+    const expected = ['Choose formatter', ['dump', 'progress-bar'], {
+        autofocus: 1,
+    }];
+    
+    t.calledWith(choose, expected);
+    t.end();
+});
+
+test('putout: cli: choose-formatter: getFormatters: formatter', async (t) => {
+    const readFile = stub().returns(stringify({
+        formatter: 'progress-bar',
+        dependencies: {
+            '@putout/formatter-dump': '^7.0.0',
+            '@putout/formatter-progress-bar': '^7.0.0',
+        },
+    }));
+    
+    const choose = stub().returns('progress-bar');
+    
+    await chooseFormatter({
+        readFile,
+        choose,
+    });
+    
+    const expected = ['Choose formatter', ['dump', 'progress-bar'], {
+        autofocus: 1,
+    }];
     
     t.calledWith(choose, expected);
     t.end();
