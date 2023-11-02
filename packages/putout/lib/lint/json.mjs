80227e2d433835e2e6a2664b04bb445698681542
coderaiser <coderaiser@cloudcmd.io>
2023-11-02 23:50:08 +0200

    feature: putout: lintJSON: add

---
+++ packages/putout/lib/lint/json.mjs
@@ -0,0 +1,13 @@
+import {
+    branch,
+    merge,
+} from '@putout/processor-json';
+import putout from '../putout.js';
+
+export const lintJSON = (rawSource, options) => {
+    const list = branch(rawSource);
+    const {source} = list[0];
+    const {code} = putout(source, options);
+    
+    return merge(rawSource, [code]);
+};
---
+++ packages/putout/lib/lint/json.spec.mjs
@@ -0,0 +1,32 @@
+import {test} from 'supertape';
+import montag from 'montag';
+import {lintJSON} from './json.mjs';
+
+const {stringify} = JSON;
+
+test('putout: lint: json', (t) => {
+    const rawSource = stringify({
+        rules: {
+            'remove-unused-variables': 'off',
+        },
+        formatter: 'json',
+    });
+    
+    const code = lintJSON(rawSource, {
+        plugins: [
+            'putout-config',
+        ],
+    });
+    
+    const expected = montag`
+        {
+            "formatter": "json",
+            "rules": {
+                "remove-unused-variables": "off"
+            }
+        }\n
+    `;
+    
+    t.equal(code, expected);
+    t.end();
+});
