b1e21f89e9bbb36ada8b0822f4c82a04e3b9684b
coderaiser <coderaiser@cloudcmd.io>
2023-11-03 22:17:38 +0200

    chore: @putout/cli-choose-formatter: v1.2.1

---
+++ packages/cli-choose-formatter/lib/json.spec.js
@@ -0,0 +1,18 @@
+import {
+    test,
+    stub,
+} from 'supertape';
+import {readJSON} from './json.js';
+
+test('putout: cli-choose-formatter', async (t) => {
+    const readFile = stub();
+    const findUp = stub();
+    
+    await readJSON('xxx', {
+        readFile,
+        findUp,
+    });
+    
+    t.notCalled(readFile);
+    t.end();
+});
