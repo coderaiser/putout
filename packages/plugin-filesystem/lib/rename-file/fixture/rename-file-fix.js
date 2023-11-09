4bcaf75cd0e3e5fd7397f977d9910d3a649ae648
coderaiser <coderaiser@cloudcmd.io>
2023-11-09 15:56:57 +0200

    test: @putout/plugin-filesystem: update

--- packages/plugin-filesystem/lib/rename-file/fixture/rename-file-fix.js
+++ packages/plugin-filesystem/lib/rename-file/fixture/rename-file-fix.js
@@ -1,18 +1,18 @@
 __putout_processor_filesystem({
-    'type': 'directory',
-    'filename': 'home/coderaiser/putout',
-    'files': [{
-        'type': 'file',
-        'filename': '/home/coderaiser/putout/readme.md',
+    "type": "directory",
+    "filename": "home/coderaiser/putout",
+    "files": [{
+        "type": "file",
+        "filename": "/home/coderaiser/putout/readme.md"
     }, {
-        'type': 'directory',
-        'filename': '/home/coderaiser/putout/lib',
-        'files': [{
-            'type': 'file',
-            'filename': '/home/coderaiser/putout/lib/putout.js',
+        "type": "directory",
+        "filename": "/home/coderaiser/putout/lib",
+        "files": [{
+            "type": "file",
+            "filename": "/home/coderaiser/putout/lib/putout.js"
         }, {
-            'type': 'file',
-            'filename': '/home/coderaiser/putout/lib/readme.md',
-        }],
-    }],
+            "type": "file",
+            "filename": "/home/coderaiser/putout/lib/readme.md"
+        }]
+    }]
 });
