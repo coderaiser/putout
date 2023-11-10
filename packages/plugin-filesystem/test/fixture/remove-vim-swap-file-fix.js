8e0bef949fa7b08d96ac94fa4201150f1d5e2031
coderaiser <coderaiser@cloudcmd.io>
2023-11-10 11:49:22 +0200

    test: @putout/plugin-filesystem: fixture

--- packages/plugin-filesystem/test/fixture/remove-vim-swap-file-fix.js
+++ packages/plugin-filesystem/test/fixture/remove-vim-swap-file-fix.js
@@ -3,13 +3,10 @@ __putout_processor_filesystem({
     "filename": "home/coderaiser/putout",
     "files": [{
         "type": "file",
-        "filename": "/home/coderaiser/putout/README.md",
+        "filename": "/home/coderaiser/putout/README.md"
     }, {
         "type": "directory",
         "filename": "/home/coderaiser/putout/lib",
-        "files": [{
-            "type": "file",
-            "filename": "/home/coderaiser/putout/lib/putout.js.swp",
-        }]
-     }]
+        "files": []
+    }]
 });
