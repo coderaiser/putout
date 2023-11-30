5bc979706d509ab03bdde799cf7e54ba195aba73
coderaiser <coderaiser@cloudcmd.io>
2023-11-30 19:27:43 +0200

    test: @putout/plugin-remove-useless-spread: fixture

--- packages/plugin-remove-useless-spread/lib/object/fixture/comment.js
+++ packages/plugin-remove-useless-spread/lib/object/fixture/comment.js
@@ -1,7 +1,6 @@
 const [error] = tryCatch(execSync, cmd, {
     stdio: [0, 1, 2],
     env: {
-        ...fn() // ZENLOAD: 'escover,mock-import',
-,
+        ...fn(), // ZENLOAD: 'escover,mock-import',
     },
 });
