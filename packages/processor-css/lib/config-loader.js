d990d9f9fd445de9a83eb33271b3165839c407c8
coderaiser <coderaiser@cloudcmd.io>
2023-12-08 23:10:19 +0200

    feature: @putout/processor: config:

--- packages/processor-css/lib/config-loader.js
+++ packages/processor-css/lib/config-loader.js
@@ -1,16 +1,33 @@
 import {createRequire} from 'node:module';
-import deepmerge from 'deepmerge';
 
 const require = createRequire(import.meta.url);
 
 export const defaultConfig = require('../stylelintrc.json');
 export const createConfigLoader = ({cosmiconfig}) => async () => {
-    const explorer = cosmiconfig('stylelint');
+    const explorer = cosmiconfig('stylelint', {
+        searchStrategy: 'project',
+    });
+    
     const result = await explorer.search();
     const newConfig = result?.config;
     
     if (!newConfig)
         return defaultConfig;
     
-    return deepmerge.all([defaultConfig, newConfig]);
+    const merged = {
+        rules: {
+            ...defaultConfig.rules,
+            ...newConfig.rules,
+        },
+        plugins: [
+            ...defaultConfig.plugins,
+            ...newConfig.plugins || [],
+        ],
+        extends: [
+            ...defaultConfig.extends,
+            ...newConfig.extends || [],
+        ],
+    };
+    
+    return merged;
 };
