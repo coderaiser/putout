ef4908d1f3a88fd12fdcffe2f750e5efb924a437
coderaiser <coderaiser@cloudcmd.io>
2023-12-21 00:54:27 +0200

    feature: @putout/plugin-filesystem: exports: read-all-files

--- packages/plugin-filesystem/test/exports.js
+++ packages/plugin-filesystem/test/exports.js
@@ -3,9 +3,11 @@
 const {test} = require('supertape');
 const toSimple = require('@putout/plugin-filesystem/to-simple');
 const fromSimple = require('@putout/plugin-filesystem/from-simple');
+const readAllFiles = require('@putout/plugin-filesystem/read-all-files');
 
 const convertFilesystemToSimpleFilesystem = require('../lib/convert-filesystem-to-simple-filesystem');
 const convertSimpleFilesystemToFilesystem = require('../lib/convert-simple-filesystem-to-filesystem');
+const readAllFilesOriginal = require('../lib/read-all-files');
 
 test('@putout/plugin-filesystem: exports: toSimple', (t) => {
     t.equal(toSimple, convertFilesystemToSimpleFilesystem);
@@ -14,5 +16,10 @@ test('@putout/plugin-filesystem: exports: toSimple', (t) => {
 
 test('@putout/plugin-filesystem: exports: fromSimple', (t) => {
     t.equal(fromSimple, convertSimpleFilesystemToFilesystem);
+    t.end();
+});
+
+test('@putout/plugin-filesystem: exports: read-all-files', (t) => {
+    t.equal(readAllFiles, readAllFilesOriginal);
     t.end();
 });
