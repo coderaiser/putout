f93ae10d3e2dc257a0da813bfe4111f085557c86
coderaiser <coderaiser@cloudcmd.io>
2024-01-18 20:16:28 +0200

    feature: putout: bin: move out subscribe

---
+++ packages/putout/bin/subscribe.mjs
@@ -0,0 +1,27 @@
+import process from 'node:process';
+import keyPress from '@putout/cli-keypress';
+import {createReport} from './report.mjs';
+
+export const subscribe = async ({cwd, args, worker, exit}) => {
+    const {isStop} = keyPress();
+    const report = await createReport({
+        args,
+        cwd,
+        exit,
+    });
+    
+    worker.on('message', async ([event, data]) => {
+        if (event !== 'progress')
+            return;
+        
+        if (isStop())
+            data.index = data.count - 1;
+        
+        process.stdout.write(await report(data));
+        
+        if (isStop())
+            worker.postMessage(['stop']);
+    });
+    
+    worker.on('exit', exit);
+};
--- packages/putout/bin/tracer.mjs
+++ packages/putout/bin/tracer.mjs
@@ -2,9 +2,8 @@
 
 import process from 'node:process';
 import {Worker} from 'node:worker_threads';
-import keyPress from '@putout/cli-keypress';
 import {parseArgs} from '../lib/cli/parse-args.js';
-import {createReport} from './report.mjs';
+import {subscribe} from './subscribe.mjs';
 
 const {cwd, exit} = process;
 const args = parseArgs(process.argv.slice(2));
@@ -15,35 +14,16 @@ if (!args.worker) {
 }
 
 const slave = new URL('./putout.mjs', import.meta.url);
-
 const worker = new Worker(slave, {
     workerData: dropInteractive(process.argv),
     stdin: true,
 });
 
-const {isStop} = keyPress();
-
-const report = await createReport({
+subscribe({
     args,
-    cwd,
+    worker,
     exit,
-});
-
-worker.on('message', async ([event, data]) => {
-    if (event !== 'progress')
-        return;
-    
-    if (isStop())
-        data.index = data.count - 1;
-    
-    process.stdout.write(await report(data));
-    
-    if (isStop())
-        worker.postMessage(['stop']);
-});
-
-worker.on('exit', (code) => {
-    exit(code);
+    cwd,
 });
 
 function dropInteractive(argv) {
