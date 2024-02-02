edb7342be2f6f92f7a4488a06f876ba16ff6b7d7
coderaiser <coderaiser@cloudcmd.io>
2024-02-02 15:03:05 +0200

    feature: @putout/plugin-filesystem: convert-js-to-json: add support of CommonJS

---
+++ packages/plugin-filesystem/lib/convert-js-to-json/convert/fixture/cjs.js
@@ -0,0 +1,34 @@
+module.exports = {
+    'root': true,
+    'parser': '@typescript-eslint/parser',
+    'env': {
+        'node': true,
+    },
+    'extends': ['eslint:recommended', './.eslintrc.base.json'],
+    'plugins': ['@nx'],
+    'rules': {
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+    },
+    'overrides': [{
+        'files': ['*.json'],
+        'parser': 'jsonc-eslint-parser',
+        'rules': {},
+    }, {
+        'files': [
+            '*.ts',
+            '*.tsx',
+            '*.js',
+            '*.jsx',
+        ],
+        'rules': {
+            '@nx/enforce-module-boundaries': ['error', {
+                'enforceBuildableLibDependency': true,
+                'allow': [],
+                'depConstraints': [{
+                    'sourceTag': '*',
+                    'onlyDependOnLibsWithTags': ['*'],
+                }],
+            }],
+        },
+    }],
+};
--- packages/plugin-filesystem/lib/convert-js-to-json/index.spec.js
+++ packages/plugin-filesystem/lib/convert-js-to-json/index.spec.js
@@ -23,3 +23,10 @@ test('@putout/plugin-filesystem: convert-js-to-json: transform', (t) => {
     });
     t.end();
 });
+
+test('@putout/plugin-filesystem: convert-js-to-json: transform: cjs', (t) => {
+    t.transformWithOptions('cjs', {
+        filename: 'package.cjs',
+    });
+    t.end();
+});
