9290bef1d005543882b91b7751852091478a65b7
coderaiser <coderaiser@cloudcmd.io>
2024-02-14 21:31:07 +0200

    feature: @putout/plugin-remove-useless-array: Traverser -> Replacer

--- packages/plugin-remove-useless-array/lib/remove-useless-array.js
+++ packages/plugin-remove-useless-array/lib/remove-useless-array.js
@@ -1,25 +1,8 @@
 'use strict';
 
-const {replaceWith} = require('putout').operator;
-
 module.exports.report = () => `Avoid array inside property accessors`;
 
-module.exports.include = () => [
-    'ArrayExpression',
-];
+module.exports.replace = () => ({
+    '__a[[__b]]': '__a[__b]',
+});
 
-module.exports.fix = (path) => {
-    replaceWith(path, path.node.elements[0]);
-};
-
-module.exports.filter = (path) => {
-    const {parentPath} = path;
-    
-    if (path === parentPath.get('object'))
-        return false;
-    
-    if (path.node.elements.length > 1)
-        return false;
-    
-    return parentPath.isMemberExpression();
-};
