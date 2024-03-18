2f73bdec770441a2930f16b0044bee7a8f0d23e4
coderaiser <coderaiser@cloudcmd.io>
2024-03-18 16:34:47 +0200

    feature: @putout/plugin-github: update-actions: docker/login-action: v3

--- packages/plugin-github/lib/update-actions/index.js
+++ packages/plugin-github/lib/update-actions/index.js
@@ -6,7 +6,7 @@ export default updateActions({
     'actions/setup-node': 'v4',
     'docker/setup-buildx-action': 'v3',
     'docker/build-push-action': 'v4',
-    'docker/login-action': 'v2',
+    'docker/login-action': 'v3',
     'docker/setup-qemu-action': 'v3',
     'coverallsapp/github-action': 'v2',
     'EndBug/add-and-commit': 'v9',
