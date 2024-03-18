f1de0c82be9b89b26c5e311bed0150b4ddd47425
coderaiser <coderaiser@cloudcmd.io>
2024-03-18 16:34:08 +0200

    feature: @putout/plugin-github: update-actions: docker/setup-buildx-action: v3

--- packages/plugin-github/lib/update-actions/index.js
+++ packages/plugin-github/lib/update-actions/index.js
@@ -4,7 +4,7 @@ export default updateActions({
     'actions/checkout': 'v4',
     'actions/cache': 'v4',
     'actions/setup-node': 'v4',
-    'docker/setup-buildx-action': 'v2',
+    'docker/setup-buildx-action': 'v3',
     'docker/build-push-action': 'v4',
     'docker/login-action': 'v2',
     'docker/setup-qemu-action': 'v3',
--- packages/plugin-github/test/fixture/set-docker-setup-buildx-version-fix.js
+++ packages/plugin-github/test/fixture/set-docker-setup-buildx-version-fix.js
@@ -19,7 +19,7 @@ __putout_processor_yaml({
                 }
             }, {
                 "name": "Set up Docker Buildx",
-                "uses": "docker/setup-buildx-action@v2"
+                "uses": "docker/setup-buildx-action@v3"
             }]
         }
     }
