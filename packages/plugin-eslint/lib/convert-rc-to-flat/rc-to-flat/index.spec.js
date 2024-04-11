1edf271aae65f394c165422b61fc7f99fe234e85
coderaiser <coderaiser@cloudcmd.io>
2024-04-11 20:45:11 +0300

    test: @putout/plugin-eslint: convert-rc-to-flat: rc-to-flat: messages

--- packages/plugin-eslint/lib/convert-rc-to-flat/rc-to-flat/index.spec.js
+++ packages/plugin-eslint/lib/convert-rc-to-flat/rc-to-flat/index.spec.js
@@ -10,12 +10,13 @@ const test = createTest(__dirname, {
     ],
 });
 
-test('eslint: convert-eslintrc-to-flat: report', (t) => {
+test('eslint: convert-rc-to-flat: rc-to-flat: report', (t) => {
     t.report('convert-eslintrc-to-flat', `Use FlatConfig instead of ESLintRC`);
     t.end();
 });
 
-test('eslint: convert-eslintrc-to-flat: transform', (t) => {
+test('eslint: convert-rc-to-flat: rc-to-flat: transform', (t) => {
     t.transform('convert-eslintrc-to-flat');
     t.end();
 });
+
