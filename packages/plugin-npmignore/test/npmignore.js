0855fa787b23ea26e1b5b647329af3658fdb1b62
coderaiser <coderaiser@cloudcmd.io>
2024-04-12 16:59:41 +0300

    fix: @putout/plugin-npmignore: message

--- packages/plugin-npmignore/test/npmignore.js
+++ packages/plugin-npmignore/test/npmignore.js
@@ -2,10 +2,14 @@ import {createTest} from '@putout/test';
 import * as npmignore from '../lib/npmignore.js';
 
 const test = createTest(import.meta.url, {
-    printer: 'putout',
     plugins: [
         ['npmignore', npmignore],
     ],
+});
+
+test('plugin-npmignore: report', (t) => {
+    t.report('npmignore', `Add dot files should be added to '.npmignore'`);
+    t.end();
 });
 
 test('plugin-npmignore: transform', (t) => {
