a239f4d44f9b4566911da15d3d213b7db246d7ab
coderaiser <coderaiser@users.noreply.github.com>
2024-05-16 11:58:56 +0000

    chore: putout: actions: lint ☘️

--- packages/plugin-eslint/lib/convert-export-match-to-declaration/index.spec.js
+++ packages/plugin-eslint/lib/convert-export-match-to-declaration/index.spec.js
@@ -6,16 +6,16 @@ const plugin = require('.');
 const test = createTest(__dirname, {
     printer: 'putout',
     plugins: [
-        ['convert-export-match-to-decleration', plugin],
+        ['convert-export-match-to-declaration', plugin],
     ],
 });
 
-test('eslint: convert-export-match-to-decleration: report', (t) => {
+test('eslint: convert-export-match-to-declaration: report', (t) => {
     t.report('convert-export-match-to-declaration', `Export 'match' at end of file in CommonJS`);
     t.end();
 });
 
-test('eslint: convert-export-match-to-decleration: transform', (t) => {
+test('eslint: convert-export-match-to-declaration: transform', (t) => {
     t.transform('convert-export-match-to-declaration');
     t.end();
 });
