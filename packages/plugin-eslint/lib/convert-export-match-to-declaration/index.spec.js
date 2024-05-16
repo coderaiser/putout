5e3b2a9205e249b115eb2aea6e0647d6ba47122a
coderaiser <coderaiser@cloudcmd.io>
2024-05-16 14:54:35 +0300

    fix: @putout/plugin-eslint: typo

--- packages/plugin-eslint/lib/convert-export-match-to-declaration/index.spec.js
+++ packages/plugin-eslint/lib/convert-export-match-to-declaration/index.spec.js
@@ -6,16 +6,16 @@ const plugin = require('.');
 const test = createTest(__dirname, {
     printer: 'putout',
     plugins: [
-        ['convert-export-match-to-declaration', plugin],
+        ['convert-export-match-to-decleration', plugin],
     ],
 });
 
-test('eslint: convert-export-match-to-declaration: report', (t) => {
+test('eslint: convert-export-match-to-decleration: report', (t) => {
     t.report('convert-export-match-to-declaration', `Export 'match' at end of file in CommonJS`);
     t.end();
 });
 
-test('eslint: convert-export-match-to-declaration: transform', (t) => {
+test('eslint: convert-export-match-to-decleration: transform', (t) => {
     t.transform('convert-export-match-to-declaration');
     t.end();
 });
