b71da018f2eb0f2367cd60ee8ef6832f1cdd8f4a
coderaiser <coderaiser@cloudcmd.io>
2024-05-19 23:53:34 +0300

    test: @putotu/plugin-remove-useless-conversion: messages

--- packages/plugin-types/lib/remove-useless-conversion/index.spec.js
+++ packages/plugin-types/lib/remove-useless-conversion/index.spec.js
@@ -6,51 +6,51 @@ const removeUselessTypeConversion = require('.');
 const test = createTest(__dirname, {
     printer: 'putout',
     plugins: [
-        ['remove-useless-type-conversion', removeUselessTypeConversion],
+        ['remove-useless-conversion', removeUselessTypeConversion],
     ],
 });
 
-test('plugin-remove-useless-type-conversion: named: report', (t) => {
+test('putout: plugin-types: remove-useless-conversion: named: report', (t) => {
     t.report('bool', 'Avoid useless type conversion');
     t.end();
 });
 
-test('plugin-remove-useless-type-conversion: named: transform', (t) => {
+test('putout: plugin-types: remove-useless-conversion: named: transform', (t) => {
     t.transform('bool');
     t.end();
 });
 
-test('plugin-remove-useless-type-conversion: named: transform: typeof', (t) => {
+test('putout: plugin-types: remove-useless-conversion: named: transform: typeof', (t) => {
     t.transform('typeof');
     t.end();
 });
 
-test('plugin-remove-useless-type-conversion: named: transform: if', (t) => {
+test('putout: plugin-types: remove-useless-conversion: named: transform: if', (t) => {
     t.transform('if');
     t.end();
 });
 
-test('plugin-remove-useless-type-conversion: named: transform: while', (t) => {
+test('putout: plugin-types: remove-useless-conversion: named: transform: while', (t) => {
     t.transform('while');
     t.end();
 });
 
-test('plugin-remove-useless-type-conversion: named: transform: for', (t) => {
+test('putout: plugin-types: remove-useless-conversion: named: transform: for', (t) => {
     t.transform('for');
     t.end();
 });
 
-test('plugin-remove-useless-type-conversion: named: transform: for-start', (t) => {
+test('putout: plugin-types: remove-useless-conversion: named: transform: for-start', (t) => {
     t.transform('for-start');
     t.end();
 });
 
-test('plugin-remove-useless-type-conversion: named: transform: for-end', (t) => {
+test('putout: plugin-types: remove-useless-conversion: named: transform: for-end', (t) => {
     t.transform('for-end');
     t.end();
 });
 
-test('plugin-remove-useless-type-conversion: named: transform: ternary', (t) => {
+test('putout: plugin-types: remove-useless-conversion: named: transform: ternary', (t) => {
     t.transform('ternary');
     t.end();
 });
