4e4f395721334543b9c8d140e48e8c55b93e4937
coderiaser <coderaiser@cloudcmd.io>
2024-06-24 19:47:22 +0300

    feature: @putout/plugin-putout: check-replace-code: split

---
+++ packages/plugin-putout/lib/check-replace-code/generate-code/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const {parse, print, transform} = require('putout');
+const tryCatch = require('try-catch');
+const pluginGenerate = require('./plugin-generate');
+
+module.exports = (rootPath, source) => {
+    const [parseError, ast] = tryCatch(parse, source, {
+        isTS: true,
+    });
+    
+    if (parseError)
+        return [parseError];
+     
+    const getVar = createVarStore(rootPath);
+    
+     transform(ast, source, {
+        rules: {
+            'generate': ['on', {
+                getVar,
+            }],
+        },
+        plugins: [
+            ['generate', pluginGenerate],
+        ]
+    });
+    
+    
+    const code = print(ast);
+    
+    return [null, code];
+};
+
+function createVarStore(path) {
+    const store = {};
+
+    return (name) => {
+        if (store[name])
+            return store[name];
+
+        store[name] = path.scope.generateUid();
+
+        return store[name];
+    };
+}
---
+++ packages/plugin-putout/lib/check-replace-code/generate-code/plugin-generate.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const {operator, types} = require('putout');
+const {
+    ObjectPattern,
+    BlockStatement,
+    Identifier,
+    ArrayPattern,
+    ObjectExpression,
+} = types;
+const {replaceWith} = operator;
+const noop = () => {};
+
+module.exports.report = noop;
+
+module.exports.include = () => [
+    'Identifier',
+    'StringLiteral',
+];
+
+module.exports.fix = (path, {options}) => {
+    const {getVar} = options;
+    const {node} = path;
+    
+    const {value, name} = node;
+    
+    if (path.isStringLiteral() && /^__[a-z]$/.test(value)) {
+        path.node.value = getVar(name);
+        return;
+    }
+    
+    if (/^__identifier__[a-z]$/.test(name)) {
+        path.node.name = getVar(name);
+        return;
+    }
+    
+    if (/^__[a-z]$/.test(name)) {
+        path.node.name = getVar(name);
+        return;
+    }
+    
+    if (/__args/.test(name)) {
+        path.node.name = getVar(name);
+        return;
+    }
+    
+    if (name === '__array') {
+        if (path.parentPath.isCallExpression())
+            return replaceWith(path, ArrayPattern([]));
+        
+        if (path.parentPath.isFunction())
+            return replaceWith(path, ArrayPattern([]));
+    }
+    
+    if (name === '__object')
+        return objectify(path);
+    
+    if (name === '__body') {
+        if (path.parentPath.isClassProperty()) {
+            const key = Identifier(getVar());
+            
+            replaceWith(path, key);
+            
+            return;
+        }
+        
+        replaceWith(path, BlockStatement([]));
+    }
+};
+
+function objectify(path) {
+    const {parentPath} = path;
+    const isAssign = parentPath.isAssignmentExpression();
+    const isVar = parentPath.isVariableDeclarator();
+    
+    if (path.parentPath.isExportDeclaration())
+        return replaceWith(path, ObjectExpression([]));
+    
+    if (path.parentPath.isCallExpression())
+        return replaceWith(path, ObjectExpression([]));
+    
+    if (isAssign && parentPath.get('right') === path)
+        return replaceWith(path, ObjectExpression([]));
+    
+    if (isVar && parentPath.get('id') === path)
+        return replaceWith(path, ObjectPattern([]));
+}
