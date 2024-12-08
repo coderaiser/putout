564692ffff30f33aaaa4083e912bfde80369ecc6
coderiaser <coderaiser@cloudcmd.io>
2024-12-08 19:42:21 +0200

    feature: scripts: set-engines: 16 -> 18

--- scripts/set-engines.js
+++ scripts/set-engines.js
@@ -8,6 +8,6 @@ console.log(process.cwd());
 const data = await readFile('./package.json', 'utf8');
 const json = parse(data);
 
-json.engines.node = '>=16';
+json.engines.node = '>=18';
 
 await writeFile('./package.json', stringify(json, null, 2) + '\n');
