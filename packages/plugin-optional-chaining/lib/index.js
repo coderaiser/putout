e1395217be1dd870a9681eac1dae767e15f60f4d
coderiaser <coderaiser@cloudcmd.io>
2024-12-13 13:23:36 +0200

    fix: @putout/plugin-optional-chaining: typo

--- packages/plugin-optional-chaining/lib/index.js
+++ packages/plugin-optional-chaining/lib/index.js
@@ -2,12 +2,12 @@
 
 const convertAssignToOptional = require('./convert-assign-to-optional');
 const convertLogicalToOptional = require('./convert-logical-to-optional');
-const convertOptionalAssignToLogical = require('./convert-optiional-assign-to-logical');
+const convertOptionalAssignToLogical = require('./convert-optional-assign-to-logical');
 const convertOptionalToLogical = require('./convert-optional-to-logical');
 
 module.exports.rules = {
     'convert-assign-to-optional': ['off', convertAssignToOptional],
     'convert-logical-to-optional': convertLogicalToOptional,
-    'convert-optiional-assign-to-logical': convertOptionalAssignToLogical,
+    'convert-optional-assign-to-logical': convertOptionalAssignToLogical,
     'convert-optional-to-logical': ['off', convertOptionalToLogical],
 };
