594f88fb3d91e8b1689016bc0b9ff62d44e05127
coderiaser <coderaiser@cloudcmd.io>
2025-01-03 13:53:58 +0200

    feature: @putout/plugin-conditions: reverse-condition: and: add

--- packages/plugin-conditions/lib/reverse-condition/index.js
+++ packages/plugin-conditions/lib/reverse-condition/index.js
@@ -4,4 +4,5 @@ module.exports.report = () => `Avoid useless '!'`;
 
 module.exports.replace = () => ({
     '!(__a > __b)': '__a <= __b',
+    '!(__a !== __b && __c === __d)': '__a === __b || __c !== __d',
 });
