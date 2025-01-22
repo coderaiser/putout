3832af3a545f05d1bd35ecdfb1608c635feaaf11
coderiaser <coderaiser@cloudcmd.io>
2025-01-22 23:34:33 +0200

    test: @putout/operator-json: coverage

--- packages/operator-json/lib/json.spec.js
+++ packages/operator-json/lib/json.spec.js
@@ -11,9 +11,9 @@ const {
     __filesystem,
     __ignore,
     __ignore_name,
-} = require('../lib/json.js');
+} = require('./json.js');
 
-test('putout: processor: json: toJS: filesystem', ({equal}) => {
+test('putout: operator: json: toJS: filesystem', ({equal}) => {
     const source = '{"hello": "world"}';
     const result = toJS(source, __filesystem);
     const expected = `__putout_processor_filesystem({"hello": "world"});\n`;
@@ -21,7 +21,7 @@ test('putout: processor: json: toJS: filesystem', ({equal}) => {
     equal(result, expected);
 });
 
-test('putout: processor: json: toJS', ({equal}) => {
+test('putout: operator: json: toJS', ({equal}) => {
     const source = '{"hello": "world"}';
     const result = toJS(source, __json);
     const expected = `__putout_processor_json({"hello": "world"});\n`;
@@ -29,7 +29,7 @@ test('putout: processor: json: toJS', ({equal}) => {
     equal(result, expected);
 });
 
-test('putout: processor: json: fromJS', ({equal}) => {
+test('putout: operator: json: fromJS', ({equal}) => {
     const source = `__putout_processor_filesystem({"hello": "world"});\n`;
     const result = fromJS(source, __filesystem);
     const expected = '{"hello": "world"}\n';
@@ -37,7 +37,7 @@ test('putout: processor: json: fromJS', ({equal}) => {
     equal(result, expected);
 });
 
-test('putout: processor: json: fromJS: strict mode', ({equal}) => {
+test('putout: operator: json: fromJS: strict mode', ({equal}) => {
     const source = `'use strict'; __putout_processor_filesystem({"hello": "world"});\n`;
     const result = fromJS(source, __filesystem);
     const expected = '{"hello": "world"}\n';
@@ -45,7 +45,7 @@ test('putout: processor: json: fromJS: strict mode', ({equal}) => {
     equal(result, expected);
 });
 
-test('putout: processor: json: fromJS: newline', ({equal}) => {
+test('putout: operator: json: fromJS: newline', ({equal}) => {
     const source = `__putout_processor_filesystem({"hello": "world"});`;
     const result = fromJS(source, __filesystem);
     const expected = '{"hello": "world"}\n';
@@ -53,7 +53,7 @@ test('putout: processor: json: fromJS: newline', ({equal}) => {
     equal(result, expected);
 });
 
-test('putout: processor: json: toJS: __yaml', ({equal}) => {
+test('putout: operator: json: toJS: __yaml', ({equal}) => {
     const source = '{"hello": "world"}';
     const result = toJS(source, __yaml);
     const expected = `__putout_processor_yaml({"hello": "world"});\n`;
@@ -61,7 +61,7 @@ test('putout: processor: json: toJS: __yaml', ({equal}) => {
     equal(result, expected);
 });
 
-test('putout: processor: json: fromJS: more newlines', ({equal}) => {
+test('putout: operator: json: fromJS: more newlines', ({equal}) => {
     const source = `__putout_processor_filesystem({"hello": "world"}\n);\n`;
     const result = fromJS(source, __filesystem);
     const expected = '{"hello": "world"}\n';
@@ -69,7 +69,7 @@ test('putout: processor: json: fromJS: more newlines', ({equal}) => {
     equal(result, expected);
 });
 
-test('putout: processor: json: fromJS: __ignore', ({equal}) => {
+test('putout: operator: json: fromJS: __ignore', ({equal}) => {
     const source = `__putout_processor_ignore([1, 2]);\n`;
     const result = fromJS(source, __ignore);
     const expected = '[1, 2]\n';
@@ -77,7 +77,7 @@ test('putout: processor: json: fromJS: __ignore', ({equal}) => {
     equal(result, expected);
 });
 
-test('putout: processor: json: fromJS: __ignore_name', ({equal}) => {
+test('putout: operator: json: fromJS: __ignore_name', ({equal}) => {
     const source = `__putout_processor_ignore([1, 2]);\n`;
     const result = fromJS(source, __ignore_name);
     const expected = '[1, 2]\n';
@@ -85,20 +85,20 @@ test('putout: processor: json: fromJS: __ignore_name', ({equal}) => {
     equal(result, expected);
 });
 
-test('putout: processor: json: isJSON', ({ok}) => {
+test('putout: operator: json: isJSON', ({ok}) => {
     const source = `__putout_processor_ignore([1, 2]);\n`;
     const result = isJSON(source);
     
     ok(result);
 });
 
-test('putout: processor: json: isJSON: false', ({notOk}) => {
+test('putout: operator: json: isJSON: false', ({notOk}) => {
     const source = `abc([1, 2]);\n`;
     const result = isJSON(source);
     
     notOk(result);
 });
 
-test('putout: processor: json: __json_name', ({equal}) => {
+test('putout: operator: json: __json_name', ({equal}) => {
     equal(__json_name, '__putout_processor_json');
 });
---
+++ packages/plugin-remove-unused-variables/test/fixture/const.js
@@ -0,0 +1,8 @@
+const a = 3,
+const b = 4;
+
+let c = 2,
+let d = 1;
+
+var e = 3,
+var f = 4;
