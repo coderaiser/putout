673671f11f2dae613461252906d2f77a732a96fe
coderiaser <coderaiser@cloudcmd.io>
2025-02-09 12:47:18 +0200

    test: @putout/plugin-react-hook-form: v6-convert-form-context-to-form-provider

--- packages/plugin-react-hook-form/lib/v6-convert-form-context-to-form-provider/index.spec.js
+++ packages/plugin-react-hook-form/lib/v6-convert-form-context-to-form-provider/index.spec.js
@@ -5,26 +5,26 @@ const convert = require('.');
 
 const test = createTest(__dirname, {
     plugins: [
-        ['convert-form-context-to-form-provider', convert],
+        ['v6-convert-form-context-to-form-provider', convert],
     ],
 });
 
-test('plugin-nextjs: convert-form-context-to-form-provider: report', (t) => {
-    t.report('convert-form-context-to-form-provider', `Use '<FormProvider/>' instead of '<FormContext/>'`);
+test('plugin-nextjs: v6-convert-form-context-to-form-provider: report', (t) => {
+    t.report('v6-convert-form-context-to-form-provider', `Use '<FormProvider/>' instead of '<FormContext/>'`);
     t.end();
 });
 
-test('plugin-nextjs: convert-form-context-to-form-provider: no report: no-form-context', (t) => {
+test('plugin-nextjs: v6-convert-form-context-to-form-provider: no report: no-form-context', (t) => {
     t.noReport('no-form-context');
     t.end();
 });
 
-test('plugin-nextjs: convert-form-context-to-form-provider: transform', (t) => {
-    t.transform('convert-form-context-to-form-provider');
+test('plugin-nextjs: v6-convert-form-context-to-form-provider: transform', (t) => {
+    t.transform('v6-convert-form-context-to-form-provider');
     t.end();
 });
 
-test('plugin-nextjs: convert-form-context-to-form-provider: transform: attributes', (t) => {
+test('plugin-nextjs: v6-convert-form-context-to-form-provider: transform: attributes', (t) => {
     t.transform('attributes');
     t.end();
 });
