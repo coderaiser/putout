26f470e88c8f18cd705ebb3eef03fe18e0ee8d3e
Alexander Vassbotn Røyne-Helgesen <alexander+github.com@phun-ky.net>
2025-02-13 10:32:41 +0100

    fix: @putout/plugin-putout-config: Add missing handler for

--- packages/plugin-putout-config/test/fixture/apply-esm.js
+++ packages/plugin-putout-config/test/fixture/apply-esm.js
@@ -1,10 +1,12 @@
+/* eslint-disable */
 __putout_processor_json({
-    "rules": {
+    rules: {
         "remove-empty/import": "on",
         "remove-empty/export": "on",
         "group-imports-by-source": "on",
         "declare-imports-first": "on",
         "remove-quotes-from-import-assertions": "on",
+        "sort-imports-by-specifiers": "on",
         "merge-duplicate-imports": "on"
-  }
+    }
 });
