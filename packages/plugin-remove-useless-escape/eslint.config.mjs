27f87f805224f50afb0cd4903cee6f1b9a6f9276
coderaiser <coderaiser@users.noreply.github.com>
2025-02-21 21:31:13 +0000

    chore: putout: actions: lint ☘️

--- packages/plugin-remove-useless-escape/eslint.config.mjs
+++ packages/plugin-remove-useless-escape/eslint.config.mjs
@@ -9,7 +9,4 @@ export const match = {
         'no-useless-escape': 'off',
     },
 };
-export default createESLintConfig([
-    safeAlign,
-    matchToFlat(match),
-]);
+export default createESLintConfig([safeAlign, matchToFlat(match)]);
