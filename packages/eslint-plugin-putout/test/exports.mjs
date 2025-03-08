9fe97183d68b2860392a8ae7ae80fd42c973bd80
coderiaser <coderaiser@cloudcmd.io>
2025-03-08 19:29:18 +0200

    fix: eslint-plugin-putout: exports: rules

--- packages/eslint-plugin-putout/test/exports.mjs
+++ packages/eslint-plugin-putout/test/exports.mjs
@@ -4,7 +4,7 @@ const {keys} = Object;
 
 test('eslint-plugin-putout: exports: rules', async (t) => {
     const {rules} = await import('eslint-plugin-putout');
-    const internal = await import('../lib/index.mjs');
+    const internal = await import('../lib/plugin.mjs');
     
     t.equal(rules, internal.rules);
     t.end();
