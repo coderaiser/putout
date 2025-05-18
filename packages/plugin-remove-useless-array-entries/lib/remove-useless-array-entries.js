72ba7f663ee3435d8a802050752e1ed01965c078
coderiaser <coderaiser@cloudcmd.io>
2025-05-18 12:37:37 +0300

    feature: @putout/plugin-remove-useless-array-entries: migrate to ESM

--- packages/plugin-remove-useless-array-entries/lib/remove-useless-array-entries.js
+++ packages/plugin-remove-useless-array-entries/lib/remove-useless-array-entries.js
@@ -1,8 +1,6 @@
-'use strict';
+export const report = () => `Remove useless '.entries()'`;
 
-module.exports.report = () => `Remove useless '.entries()'`;
-
-module.exports.replace = () => ({
+export const replace = () => ({
     'for (const [, __a] of __b.entries()) __c': 'for (const __a of __b) __c',
     'for (let [, __a] of __b.entries()) __c': 'for (let __a of __b) __c',
 });
