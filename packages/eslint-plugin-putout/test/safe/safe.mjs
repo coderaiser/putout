1fb811430c66b3056f70376ed6c2292680895705
coderiaser <coderaiser@cloudcmd.io>
2025-07-12 15:08:54 +0300

    test: eslint-plugin-putout: safe: reuse config

--- packages/eslint-plugin-putout/test/safe/safe.mjs
+++ packages/eslint-plugin-putout/test/safe/safe.mjs
@@ -3,54 +3,29 @@ import {safe, safeRules} from '../../lib/index.mjs';
 
 const test = createTest(import.meta.url, safe);
 
+const SAFE = {
+    rules: {
+        'putout/putout': ['error', {
+            ignore: ['!**/fixture'],
+            rules: {
+                ...safeRules,
+            },
+        }],
+    },
+};
+
 test('eslint-plugin-putout: safe: no-remove-useless-declaration', async ({process}) => {
-    await process('remove-useless-declaration', {
-        rules: {
-            'putout/putout': ['error', {
-                ignore: ['!**/fixture'],
-                rules: {
-                    ...safeRules,
-                },
-            }],
-        },
-    });
+    await process('remove-useless-declaration', SAFE);
 });
 
 test('eslint-plugin-putout: safe: merge-duplicate-functions: off', async ({noProcess}) => {
-    await noProcess('merge-duplicate-functions', {
-        rules: {
-            'putout/putout': ['error', {
-                ignore: ['!**/fixture'],
-                rules: {
-                    ...safeRules,
-                },
-            }],
-        },
-    });
+    await noProcess('merge-duplicate-functions', SAFE);
 });
 
 test('eslint-plugin-putout: safe: remove-useless-push: off', async ({noProcess}) => {
-    await noProcess('remove-useless-push', {
-        rules: {
-            'putout/putout': ['error', {
-                ignore: ['!**/fixture'],
-                rules: {
-                    ...safeRules,
-                },
-            }],
-        },
-    });
+    await noProcess('remove-useless-push', SAFE);
 });
 
 test('eslint-plugin-putout: safe: convert-template-to-string: off', async ({noProcess}) => {
-    await noProcess('convert-template-to-string', {
-        rules: {
-            'putout/putout': ['error', {
-                ignore: ['!**/fixture'],
-                rules: {
-                    ...safeRules,
-                },
-            }],
-        },
-    });
+    await noProcess('convert-template-to-string', SAFE);
 });
