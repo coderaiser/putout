cb60659704b24b7eff3f43c9c949feb73981bb19
coderiaser <coderaiser@cloudcmd.io>
2025-07-19 00:25:06 +0300

    feature: eslint-plugin-putout: jsx: @stylistic/jsx-indent -> @stylistic/indent

--- packages/eslint-plugin-putout/lib/jsx.mjs
+++ packages/eslint-plugin-putout/lib/jsx.mjs
@@ -1,11 +1,12 @@
 import stylistic from '@stylistic/eslint-plugin';
 import react from 'eslint-plugin-react';
+import {rules} from '@putout/eslint-config';
 
 export const jsx = {
     name: 'putout: jsx',
     rules: {
+        ...rules,
         '@stylistic/no-extra-parens': 'off',
-        '@stylistic/jsx-indent': 'error',
         '@stylistic/jsx-wrap-multilines': ['error', {
             arrow: 'parens-new-line',
             return: 'parens-new-line',
