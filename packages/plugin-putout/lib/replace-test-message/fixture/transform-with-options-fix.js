eac352dd8b263c256bab5e32a7094feae5c19354
coderiaser <coderaiser@cloudcmd.io>
2025-07-25 22:11:34 +0300

    feature: @putout/plugin-putout: replace-test-message: transform-with-options: no transform with options

--- packages/plugin-putout/lib/replace-test-message/fixture/transform-with-options-fix.js
+++ packages/plugin-putout/lib/replace-test-message/fixture/transform-with-options-fix.js
@@ -4,3 +4,10 @@ test('bundler-fasm: apply-debug: transform with options', (t) => {
     });
     t.end();
 });
+
+test('bundler-fasm: apply-debug: transform with options', (t) => {
+    t.transformWithOptions('apply-debug-options', {
+        debug: true,
+    });
+    t.end();
+});
--- packages/plugin-putout/lib/replace-test-message/fixture/transform-with-options.js
+++ packages/plugin-putout/lib/replace-test-message/fixture/transform-with-options.js
@@ -4,3 +4,10 @@ test('bundler-fasm: apply-debug: transform', (t) => {
     });
     t.end();
 });
+
+test('bundler-fasm: apply-debug: no transform with options', (t) => {
+    t.transformWithOptions('apply-debug-options', {
+        debug: true,
+    });
+    t.end();
+});
