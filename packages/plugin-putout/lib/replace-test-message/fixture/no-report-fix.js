a8bd4d2ed693ba297c6e1e727f73193ce6847394
coderiaser <coderaiser@cloudcmd.io>
2025-09-07 12:32:13 +0300

    feature: @putout/plugin-putout: replace-test-message: no report with options -> no report

--- packages/plugin-putout/lib/replace-test-message/fixture/no-report-fix.js
+++ packages/plugin-putout/lib/replace-test-message/fixture/no-report-fix.js
@@ -2,3 +2,8 @@ test.only('lib: show-menu: no report: hide-menu', (t) => {
     t.noReport('no-menu');
     t.end();
 });
+
+test('menu: hide-submenu: no report: no-menu', (t) => {
+    t.noReport('no-menu');
+    t.end();
+});
--- packages/plugin-putout/lib/replace-test-message/fixture/no-report.js
+++ packages/plugin-putout/lib/replace-test-message/fixture/no-report.js
@@ -2,3 +2,9 @@ test.only('lib: show-menu: transform with options: hide-menu', (t) => {
     t.noReport('no-menu');
     t.end();
 });
+
+test('menu: hide-submenu: no report with options: no-menu', (t) => {
+    t.noReport('no-menu');
+    t.end();
+});
+
