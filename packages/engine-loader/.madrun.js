240fcc192745778177197bc1f71d36bc78827fb0
coderiaser <coderaiser@cloudcmd.io>
2026-01-03 14:35:35 +0200

    fix: @putout/engine-loader: rules -> transforms

--- packages/engine-loader/.madrun.js
+++ packages/engine-loader/.madrun.js
@@ -3,7 +3,7 @@ import {run} from 'madrun';
 export default {
     'test': () => `tape test/*.js 'lib/**/*.spec.js' 'rules/**/*.spec.js'`,
     'watch:test': async () => `nodemon -w lib -w test -x "${await run('test')}"`,
-    'lint': () => `putout . --rulesdir rules`,
+    'lint': () => `putout . --rulesdir transforms`,
     'fresh:lint': () => run('lint', '--fresh'),
     'lint:fresh': () => run('lint', '--fresh'),
     'fix:lint': () => run('lint', '--fix'),
