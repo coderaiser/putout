144ca9bfe8432b67be4837c3e5c2249242410773
coderiaser <coderaiser@cloudcmd.io>
2026-01-10 00:39:45 +0200

    test: @putout/plugin-putout: coverage

--- packages/plugin-putout/lib/check-replace-code/fixture/namespace.js
+++ packages/plugin-putout/lib/check-replace-code/fixture/namespace.js
@@ -1,5 +1,3 @@
 export const replace = () => ({
-    'module __identifier {__body}': 'declare namespace __identifier {__body}',
-    'declare module __identifier__a {__body}': 'declare namespace __identifier__a {__body}',
-    'module __identifier__a {__body}': 'namespace __identifier__a {__body}',
+    'namespace __identifier {__body}': 'declare namespace __identifier {__body}',
 });
