73826cd2729886f0c8e18296a4e0d9f1164c6ce4
coderiaser <coderaiser@cloudcmd.io>
2026-01-19 18:15:28 +0200

    test: @putout/plugin-package-json: coverage

--- packages/plugin-package-json/test/fixture/remove-imports-nesting-fix.js
+++ packages/plugin-package-json/test/fixture/remove-imports-nesting-fix.js
@@ -1,14 +1,8 @@
 __putout_processor_json({
     "imports": {
-        "#get-imports": {
-          "default": "./lib/shorten-imported-file/get-imports/index.js"
-        },
-        "#change-imports": {
-          "default": "./lib/resolve-imported-file/change-imports/index.js"
-        },
-        "#find-package": {
-          "default": "./lib/apply-privately-imported-file/find-package.js"
-        }
+        "#get-imports": "./lib/shorten-imported-file/get-imports/index.js",
+        "#change-imports": "./lib/resolve-imported-file/change-imports/index.js",
+        "#find-package": "./lib/apply-privately-imported-file/find-package.js"
     }
 });
 
@@ -17,6 +11,6 @@ __putout_processor_json({});
 __putout_processor_json({
     "imports": {
         "#get-imports": "./lib/shorten-imported-file/get-imports/index.js",
-        "#get-imports": 5,
+        "#get-imports": 5
     }
 });
