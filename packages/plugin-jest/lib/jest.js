1deccef7e0a6720cc36048d399842d7c6b1ca61f
coderiaser <coderaiser@cloudcmd.io>
2026-01-28 14:16:16 +0200

    feature: @putout/plugin-jest: migrate to ESM

--- packages/plugin-jest/lib/jest.js
+++ packages/plugin-jest/lib/jest.js
@@ -1,8 +1,6 @@
-'use strict';
+export const report = () => 'Latest Jest API should be used';
 
-module.exports.report = () => 'Latest Jest API should be used';
-
-module.exports.replace = () => ({
+export const replace = () => ({
     'jest.addMatchers(__args)': 'expect.extend(__args)',
     'jest.resetModuleRegistry(__args)': 'jest.resetModules(__args)',
     'jest.runTimersToTime(__args)': 'jest.advanceTimersByTime(__args)',
