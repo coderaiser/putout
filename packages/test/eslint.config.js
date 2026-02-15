3985858d5f5ceb09e35ab5d8263e1c9a0d047135
coderiaser <coderaiser@cloudcmd.io>
2026-02-15 14:02:20 +0200

    chore: @putout/test: rm @putout/eslint-flat

--- packages/test/eslint.config.js
+++ packages/test/eslint.config.js
@@ -1,11 +1,3 @@
-import {matchToFlat} from '@putout/eslint-flat';
 import {safeAlign} from 'eslint-plugin-putout';
-import {defineConfig} from 'eslint/config';
 
-export const match = {
-    'rules/**/*.js': {
-        'import/no-extraneous-dependencies': 'off',
-    },
-};
-
-export default defineConfig([safeAlign, matchToFlat(match)]);
+export default safeAlign;
