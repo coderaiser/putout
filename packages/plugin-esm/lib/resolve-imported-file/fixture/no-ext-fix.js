fea4dd87768305c0feb1c22da18be9acb8617197
coderiaser <coderaiser@cloudcmd.io>
2026-02-16 22:05:38 +0200

    chore: @putout/plugin-esm: v10.0.0

---
+++ packages/plugin-esm/lib/resolve-imported-file/fixture/no-ext-fix.js
@@ -0,0 +1,7 @@
+__putout_processor_filesystem([
+    "/",
+    "/lib/",
+    ["/lib/help.js", "CiAgICAgICAgZXhwb3J0IGNvbnN0IGhlbHAgPSAnaGVsbG8nOwogICAg"],
+    "/test/",
+    ["/test/cli.js", "aW1wb3J0IHtoZWxwfSBmcm9tICcuLi9saWIvaGVscC5qcyc7Cg=="]
+]);
