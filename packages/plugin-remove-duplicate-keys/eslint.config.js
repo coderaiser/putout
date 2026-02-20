bfc6eebc38c4c9e9893445f0bd00e60ffdbdcb5a
coderiaser <coderaiser@cloudcmd.io>
2026-02-20 20:13:26 +0200

    feature: @putout/plugin-remove-duplicate-keys: drop support of 🐊 < 42

--- packages/plugin-remove-duplicate-keys/eslint.config.js
+++ packages/plugin-remove-duplicate-keys/eslint.config.js
@@ -1,10 +1,3 @@
 import {safeAlign} from 'eslint-plugin-putout';
-import {matchToFlat} from '@putout/eslint-flat';
-import {defineConfig} from 'eslint/config';
 
-export const match = {
-    '*.md{js}': {
-        'no-dupe-keys': 'off',
-    },
-};
-export default defineConfig([safeAlign, matchToFlat(match)]);
+export default safeAlign;
