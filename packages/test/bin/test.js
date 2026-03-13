f61d5d3a77f74f8f53008515ada23db45ad31682
coderiaser <coderaiser@cloudcmd.io>
2026-03-13 11:29:56 +0200

    feature: @putout/test: add type-checker

--- packages/test/bin/test.js
+++ packages/test/bin/test.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
+import process from 'node:process';
 import 'supertape/bin/supertape';
+import {callWhenTestsEnds} from 'supertape';
+import {getCoverage} from '@putout/printer/type-checker/instrument';
+import {report} from '@putout/printer/type-checker/report';
+
+const {log} = console;
+
+callWhenTestsEnds('PUTOUT_INSTRUMENT', () => {
+    const coverage = getCoverage();
+    const [code, message] = report(coverage);
+    
+    log(message);
+    
+    process.exitCode = code;
+});
