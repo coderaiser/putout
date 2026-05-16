58457aeb1f0f3689021a2920871b5f61d41c66c3
coderiaser <coderaiser@cloudcmd.io>
2026-05-16 15:14:10 +0300

    chore: @putout/plugin-esm: madrun: SUPERC8_RESPONSIVE

--- packages/plugin-esm/.madrun.js
+++ packages/plugin-esm/.madrun.js
@@ -1,5 +1,9 @@
 import {run} from 'madrun';
 
+const env = {
+    SUPERC8_RESPONSIVE: 1,
+};
+
 export default {
     'prepublishOnly': () => run(['lint', 'test']),
     'test': () => `tape 'test/*.js' 'lib/**/*.spec.js'`,
@@ -8,6 +12,6 @@ export default {
     'fresh:lint': () => run('lint', '--fresh'),
     'lint:fresh': () => run('lint', '--fresh'),
     'fix:lint': () => run('lint', '--fix'),
-    'coverage': async () => `c8 ${await run('test')}`,
+    'coverage': async () => [env, `c8 ${await run('test')}`],
     'report': () => 'c8 report --reporter=lcov',
 };
