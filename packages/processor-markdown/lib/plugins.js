61429aa635a1c487808b361c1bf7b82d20262686
coderaiser <coderaiser@users.noreply.github.com>
2026-06-13 23:05:23 +0000

    chore: putout: actions: lint ☘️

--- packages/processor-markdown/lib/plugins.js
+++ packages/processor-markdown/lib/plugins.js
@@ -1,8 +1,4 @@
 import * as removeDependenciesStatusBadge from './rules/remove-dependencies-status-badge/index.js';
 import * as removeTrailingWhitespacesFromHeading from './rules/remove-trailing-whitespaces-from-heading/index.js';
 
-export const plugins = [
-    removeDependenciesStatusBadge,
-    removeTrailingWhitespacesFromHeading,
-];
-
+export const plugins = [removeDependenciesStatusBadge, removeTrailingWhitespacesFromHeading];
