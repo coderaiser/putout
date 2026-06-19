56a6b5e229ac5cf7e417e1c3ccb3a13505255aef
coderaiser <coderaiser@users.noreply.github.com>
2026-06-19 09:16:28 +0000

    chore: putout: actions: lint ☘️

--- packages/compare/test/errors.ts
+++ packages/compare/test/errors.ts
@@ -12,26 +12,19 @@ import {
 
 // THROWS Expected 2-4 arguments, but got 1.
 compare(5);
-
 // THROWS Expected 2-3 arguments, but got 1.
 compareAny(5);
-
 // THROWS Expected 2-3 arguments, but got 1.
 compareAll(5);
-
 // THROWS Expected 2 arguments, but got 1.
 getTemplateValues(5);
 // THROWS Argument of type 'number' is not assignable to parameter of type '{ waysTo: Record<string, string[]>; values: Record<string, Node>; path: NodePath_Final; }'.
 setValues(5);
-
 // THROWS Argument of type 'number' is not assignable to parameter of type '{ waysFrom: Record<string, string[]>; node: Node; }'
 getValues(5);
-
 // THROWS Argument of type 'number' is not assignable to parameter of type 'Node'
 findVarsWays(5);
-
 // THROWS Argument of type 'number' is not assignable to parameter of type 'string'
 parseTemplate(5);
-
 // THROWS Argument of type 'number' is not assignable to parameter of type 'string'
 isTemplate(5);
