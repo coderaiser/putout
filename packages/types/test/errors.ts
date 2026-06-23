fd09134bf463906385453bd6e9a730557460b944
coderaiser <coderaiser@users.noreply.github.com>
2026-06-23 14:39:23 +0000

    chore: putout: actions: lint ☘️

--- packages/types/test/errors.ts
+++ packages/types/test/errors.ts
@@ -10,15 +10,21 @@ import {
 
 // THROWS Expected 2-4 arguments, but got 1
 compare('hello');
+
 // THROWS Expected 2 arguments, but got 1
 traverse(5);
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'string'
 toJS(5);
+
 // THROWS Expected 2 arguments, but got 1
 hasTagName(5);
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'IgnoreOptions'
 ignore(5);
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'AddArgsOptions'
 addArgs(5);
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'NodePath_Final'
 remove(5);
