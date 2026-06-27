44cddac19bd1d55fda40c6033d6c6a047121fe63
coderaiser <coderaiser@users.noreply.github.com>
2026-06-27 15:33:19 +0000

    chore: putout: actions: lint ☘️

--- packages/operator-regexp/test/errors.ts
+++ packages/operator-regexp/test/errors.ts
@@ -5,16 +5,22 @@ import {
 } from '../lib/regexp.js';
 
 const noop = () => {};
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'RegExp'
 isSimpleRegExp(5);
+
 // THROWS Expected 1 arguments, but got 0
 isSimpleRegExp();
+
 // THROWS Argument of type 'number' is not assignable to parameter of type '{ pattern: string; }'
 getStringFromRegExp(5);
+
 // THROWS Expected 1 arguments, but got 0
 getStringFromRegExp();
+
 // THROWS Expected 2 arguments, but got 0
 transformRegExp();
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'string'
 transformRegExp(5, {
     report: () => '',
--- packages/putout/test/operator.errors.ts
+++ packages/putout/test/operator.errors.ts
@@ -17,29 +17,42 @@ const {
 
 // THROWS Expected 2 arguments, but got 1.
 replaceWith(1);
+
 // THROWS Expected 2-4 arguments, but got 1.
 compare('hello');
+
 // THROWS Expected 2 arguments, but got 1.
 traverse(1);
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'string'
 toJS(5);
+
 // THROWS Expected 2 arguments, but got 1
 hasTagName(5);
+
 // THROWS Expected 2 arguments, but got 1.
 replaceWith(1);
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'AddArgsOptions'
 addArgs(5);
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'DeclareOptions'
 declare(5);
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'string'
 isKeyword(5);
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'NodePath_Final'.
 findFile(5, 'hello');
+
 // THROWS Expected 1 arguments, but got 2.
 matchFiles(5, 'hello');
+
 // THROWS Expected 2 arguments, but got 1
 findFileUp(5);
+
 // THROWS Argument of type 'number' is not assignable to parameter of type 'RegExp'
 isSimpleRegExp(5);
+
 // THROWS Expected 1 arguments, but got 0
 isSimpleRegExp();
