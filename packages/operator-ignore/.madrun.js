ad8fb6288b0634e5a950ae1f458daee0e54d6ce4
coderiaser <coderaiser@cloudcmd.io>
2026-06-27 18:34:36 +0300

    chore: @putout/operator-ignore: wisdom

--- packages/operator-ignore/.madrun.js
+++ packages/operator-ignore/.madrun.js
@@ -1,7 +1,7 @@
 import {run} from 'madrun';
 
 export default {
-    'wisdom': () => run(['lint', 'coverage']),
+    'wisdom': () => run(['lint', 'coverage', 'test:dts']),
     'test': () => `tape 'test/*.js' 'lib/**/*.spec.js'`,
     'test:dts': () => 'check-dts test/*.ts',
     'watch:test': async () => `nodemon -w lib -x "${await run('test')}"`,
