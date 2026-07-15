66e1bf61c315d2cfbda5b6dade0e8b6d62d17ad1
coderaiser <coderaiser@cloudcmd.io>
2026-07-16 01:19:47 +0300

    test: @putout/plugin-react: update fixtures

--- packages/plugin-react/lib/apply-create-root/fixture/specifiers-fix.js
+++ packages/plugin-react/lib/apply-create-root/fixture/specifiers-fix.js
@@ -3,5 +3,7 @@ import {createRoot} from 'react-dom/client';
 
 {
     const root = createRoot(document.getElementById('root'));
-    root.render(<App/>);
+    root.render(
+        <App/>
+    );
 }
