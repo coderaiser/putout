4124f1c428bef1525ca2effd9dca024bac986cd8
coderiaser <coderaiser@cloudcmd.io>
2026-08-27 22:10:36 +0300

    feature: @putout/operator-json: types: __sql_name

--- packages/operator-json/lib/json.d.ts
+++ packages/operator-json/lib/json.d.ts
@@ -12,6 +12,7 @@ export const __toml_name: '__putout_processor_toml';
 export const __filesystem_name: '__putout_processor_filesystem';
 export const __ignore_name: '__putout_processor_ignore';
 export const __markdown_name: '__putout_processor_markdown';
+export const __sql_name: '__putout_processor_sql';
 
 export const __json: string;
 export const __yaml: string;
---
+++ packages/operator-json/test/types.ts
@@ -0,0 +1,5 @@
+import {__sql_name} from '../lib/json.js';
+
+const fn = (a: string) => a;
+
+fn(__sql_name);
